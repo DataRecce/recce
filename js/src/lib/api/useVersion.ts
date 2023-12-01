e3dd6f77b4026713ef0834452e59f295dafe0a66
Ching Yi, Chan <qrtt1@infuseai.io>
2023-12-01 11:00:31 +0800

    Add version number
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

---
+++ js/src/lib/api/useVersion.ts
@@ -0,0 +1,22 @@
+import {useState, useEffect} from 'react';
+import axios from 'axios';
+
+
+export function useVersionNumber() {
+    const [version, setVersion] = useState('');
+
+    useEffect(() => {
+        async function fetchVersion() {
+            try {
+                const response = await axios.get('/api/version');
+                setVersion(response.data);
+            } catch (error) {
+                console.error('Error fetching version number:', error);
+            }
+        }
+
+        fetchVersion();
+    }, []);
+
+    return version;
+}
