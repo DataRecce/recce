f10f658f9c853e4a2646aaacee02b7d756f22ac4
popcorny <celu@infuseai.io>
2023-12-12 15:15:43 +0800

    Add checks API
    Signed-off-by: popcorny <celu@infuseai.io>

---
+++ js/src/lib/api/checks.ts
@@ -0,0 +1,48 @@
+import { Run, RunParams, RunType, getRun } from "./runs";
+import _ from "lodash";
+
+interface Check {
+  check_id: string;
+  name: string;
+  description?: string;
+  type: RunType;
+  params?: RunParams;
+  last_run?: Run;
+}
+
+const checks: Check[] = [];
+
+export async function createCheckByRun(runId: string) {
+  const run = await getRun(runId);
+  const checkId = Math.random()
+    .toString(36)
+    .substring(2, 16 + 2);
+
+  if (!run) {
+    throw Error(`cannot find run: ${runId}`);
+  }
+
+  const check: Check = {
+    check_id: checkId,
+    name: `Check ${new Date().toString}`,
+    type: run.type,
+    params: run.params,
+    last_run: run,
+  };
+
+  checks.push(check);
+
+  return check;
+}
+
+export async function listChecks() {
+  return checks;
+}
+
+export async function getCheck(checkId: string) {
+  return _.find(checks, (check) => check.check_id === checkId);
+}
+
+export async function deleteCheck(checkId: string) {
+  _.remove(checks, (check) => check.check_id === checkId);
+}
--- js/src/lib/api/runs.ts
+++ js/src/lib/api/runs.ts
@@ -1,11 +1,16 @@
 import { useQuery } from "@tanstack/react-query";
 import { QueryDiffParams, QueryDiffResult, runQueryDiff } from "./adhocQuery";
+import _ from "lodash";
+import { getCheck } from "./checks";
 
 export type RunType = "query_diff";
+export type RunParams = object;
 
 export interface Run {
   run_id: string;
+  check_id?: string;
   type: RunType;
+  params?: RunParams;
   result: QueryDiffResult;
 }
 
@@ -13,10 +18,10 @@ const runs: Run[] = [];
 
 interface SubmitRunInput {
   type: RunType;
-  params: object;
+  params?: RunParams;
 }
 
-export async function submitRun(input: SubmitRunInput): Promise<Run> {
+export async function submitRun(input: SubmitRunInput) {
   const type = input.type;
   const run_id = Math.random()
     .toString(36)
@@ -25,14 +30,31 @@ export async function submitRun(input: SubmitRunInput): Promise<Run> {
   if (type === "query_diff") {
     const result = await runQueryDiff(input.params as QueryDiffParams);
 
-    return {
+    const run: Run = {
       run_id,
       type,
+      params: input.params,
       result,
     };
+
+    runs.push(run);
+
+    return run;
   } else {
     throw Error(`Wrong run type ${input.type}`);
   }
+}
+
+export async function submitRunFromCheck(checkId: string): Promise<Run> {
+  const check = await getCheck(checkId);
+  if (!check) {
+    throw Error(`check not found: ${checkId}`);
+  }
+
+  const run = await submitRun({ type: check.type, params: check.params });
+  run.check_id = checkId;
+  check.last_run = run;
+  return run;
 }
 
 export function useSubmitRun(input: SubmitRunInput, queryKey: any[]) {
@@ -42,4 +64,8 @@ export function useSubmitRun(input: SubmitRunInput, queryKey: any[]) {
     retry: false,
     enabled: false, // never auto run
   });
+}
+
+export async function getRun(runId: string) {
+  return _.find(runs, (run) => run.run_id === runId);
 }
