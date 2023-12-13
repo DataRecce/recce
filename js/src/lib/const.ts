bb18fd58b25cddef5f8e2d10e087512259cd83f4
Kent Huang <kent@infuseai.io>
2023-12-13 11:14:38 +0800

    [Fix] react coding issue
    Signed-off-by: Kent Huang <kent@infuseai.io>

--- js/src/lib/const.ts
+++ js/src/lib/const.ts
@@ -1,1 +1,2 @@
-export const PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL || location.origin;
+'use client'
+export const PUBLIC_API_URL = process.env.NEXT_PUBLIC_API_URL || (typeof window !== 'undefined') ? window.location.origin : '';
