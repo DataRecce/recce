a18b50093d3c905e98d1c60e21ebf097e9cd2020
Kent Huang <kent@infuseai.io>
2023-12-18 21:45:33 +0800

    [Fix] remove unused code
    Signed-off-by: Kent Huang <kent@infuseai.io>

--- js/src/lib/api/cacheKeys.ts
+++ js/src/lib/api/cacheKeys.ts
@@ -1,6 +1,5 @@
 export const cacheKeys = {
   rowCount: (model: string) => ["row_count", model],
-  adhocQuery: () => ["adhoc_query"],
   checks: () => ["checks", "list"],
   check: (checkId: string) => ["checks", checkId],
 };
