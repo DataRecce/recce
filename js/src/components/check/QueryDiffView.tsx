adbc056dcd7377db58faece38ebbd7d004a2b05a
Kent Huang <kent@infuseai.io>
2024-01-05 11:52:23 +0800

    [PoC] Copy the query diff result as image and save to clipboard
    Signed-off-by: Kent Huang <kent@infuseai.io>

--- js/src/components/check/QueryDiffView.tsx
+++ js/src/components/check/QueryDiffView.tsx
@@ -1,3 +1,4 @@
+import { RefObject, useRef, useState } from "react";
 import {
   Accordion,
   AccordionButton,
@@ -5,17 +6,63 @@ import {
   AccordionItem,
   AccordionPanel,
   Box,
+  HStack,
+  Spacer,
+  Button,
+  useToast,
 } from "@chakra-ui/react";
 import SqlEditor from "@/components/query/SqlEditor";
 import { QueryDiffDataGrid } from "@/components/query/QueryDiffDataGrid";
 import { QueryDiffResult } from "@/lib/api/adhocQuery";
 import { Check } from "@/lib/api/checks";
+import { CopyIcon } from "@chakra-ui/icons";
+import { toPng } from "html-to-image";
 
 interface QueryDiffViewProp {
   check: Check;
 }
 
+const convertToImage = async (ref: any) => {
+  try {
+    const dataUrl = await toPng(ref.current);
+    return dataUrl;
+  } catch (error) {
+    console.error('Error converting to image', error);
+  }
+};
+
 export function QueryDiffView({ check }: QueryDiffViewProp) {
+  const ref = useRef<HTMLDivElement>();
+  const clipboardToast = useToast();
+  const [isClipboardHighlight, setClipboardHighlight]  = useState(false)
+
+  const saveToClipboard = async () => {
+    const dataUrl = await convertToImage(ref);
+    if (!dataUrl) {
+      return;
+    }
+    try {
+      const response = await fetch(dataUrl);
+      const blob = await response.blob();
+      await navigator.clipboard.write([
+        new ClipboardItem({
+          [blob.type]: blob
+        })
+      ]);
+      console.log('Image copied.');
+      clipboardToast({
+        description: `Copied the query result as an image to clipboard`,
+        status: "info",
+        variant: "left-accent",
+        position: "bottom",
+        duration: 5000,
+        isClosable: true,
+      });
+    } catch (error) {
+      console.error('Error copying image to clipboard', error);
+    }
+  };
+
   return (
     <>
       <Accordion defaultIndex={[]} allowToggle>
@@ -36,7 +83,13 @@ export function QueryDiffView({ check }: QueryDiffViewProp) {
         </AccordionItem>
       </Accordion>
 
-      <Box flex="1" style={{ contain: "size" }}>
+      <Box
+        ref={ref as RefObject<HTMLInputElement>}
+        flex="1"
+        style={{ contain: "size" }}
+        shadow={isClipboardHighlight ? "dark-lg" : undefined}
+        transition="box-shadow 0.5s ease-in-out"
+      >
         {check?.type === "query_diff" && (
           <QueryDiffDataGrid
             run={check?.last_run}
@@ -44,6 +97,17 @@ export function QueryDiffView({ check }: QueryDiffViewProp) {
           />
         )}
       </Box>
+      <HStack padding="16px">
+        <Spacer />
+        <Button
+          size="sm"
+          leftIcon={<CopyIcon/>}
+          onMouseEnter={() => setClipboardHighlight(true)}
+          onMouseLeave={() => setClipboardHighlight(false)}
+          onClick={saveToClipboard}>
+          Copy to Clipboard
+        </Button>
+      </HStack>
     </>
   );
 }
