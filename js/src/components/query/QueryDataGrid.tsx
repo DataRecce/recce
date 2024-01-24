ba71d7eda25b0ea03657cacee161aed268786e55
popcorny <celu@infuseai.io>
2024-01-24 15:35:55 +0800

    Support pinning columns
    Signed-off-by: popcorny <celu@infuseai.io>

--- js/src/components/query/QueryDataGrid.tsx
+++ js/src/components/query/QueryDataGrid.tsx
@@ -7,6 +7,8 @@ import {
   Box,
   Button,
   Center,
+  Flex,
+  Icon,
   Spinner,
   VStack,
 } from "@chakra-ui/react";
@@ -14,6 +16,7 @@ import { CSSProperties, useMemo, useState } from "react";
 import { DataFrame, Run } from "@/lib/api/types";
 import { ScreenshotDataGrid } from "../data-grid/ScreenshotDataGrid";
 import { defaultRenderCell } from "./querydiff";
+import { VscPin, VscPinned } from "react-icons/vsc";
 
 interface QueryDataGridProps {
   style?: CSSProperties;
@@ -24,16 +27,73 @@ interface QueryDataGridProps {
   enableScreenshot?: boolean;
 }
 
-function toDataGrid(result: DataFrame) {
-  const columns: Column<any, any>[] = result.schema.fields.map((field) => {
-    return {
-      key: field.name,
-      name: field.name === "index" ? "" : field.name,
-      width: field.name === "index" ? 10 : "auto",
-      cellClass: field.name === "index" ? "index-column" : undefined,
-      renderCell: defaultRenderCell,
-    };
+interface QueryDataGridOptions {
+  pinnedColumns?: string[];
+  onPinnedColumnsChange?: (pinnedColumns: string[]) => void;
+}
+
+function DataFrameColumnHeader({
+  name,
+  pinnedColumns = [],
+  onPinnedColumnsChange = () => {},
+}: { name: string } & QueryDataGridOptions) {
+  const isPinned = pinnedColumns.includes(name);
+
+  const handleUnpin = () => {
+    const newPinnedColumns = pinnedColumns.filter((item) => item !== name);
+
+    onPinnedColumnsChange(newPinnedColumns);
+  };
+
+  const handlePin = () => {
+    const newPinnedColumns = [...pinnedColumns, name];
+
+    onPinnedColumnsChange(newPinnedColumns);
+  };
+
+  return (
+    <Flex className="grid-header" alignItems="center">
+      <Box flex={1}>{name}</Box>
+
+      <Icon
+        className={isPinned ? "unpin-icon" : "pin-icon"}
+        visibility={isPinned ? "visible" : "hidden"}
+        cursor="pointer"
+        as={isPinned ? VscPinned : VscPin}
+        onClick={isPinned ? handleUnpin : handlePin}
+      />
+    </Flex>
+  );
+}
+
+function toDataGrid(result: DataFrame, options: QueryDataGridOptions) {
+  const columns: Column<any, any>[] = [];
+  const pinnedColumns = options.pinnedColumns || [];
+  const fields = result.schema.fields;
+  const toColumn = (name: string) => ({
+    key: name,
+    name: <DataFrameColumnHeader name={name} {...options} />,
+    width: "auto",
+    renderCell: defaultRenderCell,
   });
+
+  columns.push({
+    key: "index",
+    name: "",
+    width: 10,
+    cellClass: "index-column",
+  });
+
+  pinnedColumns.forEach((name) => {
+    columns.push(toColumn(name));
+  });
+
+  fields
+    .filter((field) => field.name !== "index")
+    .filter((field) => !pinnedColumns.includes(field.name))
+    .forEach(({ name }) => {
+      columns.push(toColumn(name));
+    });
 
   return { columns, rows: result.data };
 }
@@ -46,14 +106,18 @@ export const QueryDataGrid = ({
   enableScreenshot,
 }: QueryDataGridProps) => {
   const [isAborting, setAborting] = useState(false);
+  const [pinnedColumns, setPinnedColumns] = useState<string[]>([]);
   const dataframe = run?.result?.result;
   const gridData = useMemo(() => {
     if (isFetching || !dataframe) {
       return { rows: [], columns: [] };
     }
 
-    return toDataGrid(dataframe);
-  }, [isFetching, dataframe]);
+    return toDataGrid(dataframe, {
+      pinnedColumns,
+      onPinnedColumnsChange: setPinnedColumns,
+    });
+  }, [isFetching, dataframe, pinnedColumns, setPinnedColumns]);
 
   const handleCancel = () => {
     setAborting(true);
