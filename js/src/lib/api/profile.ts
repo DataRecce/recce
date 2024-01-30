46eb31f09417f83fb03c2216dcdf0c430410d857
popcorny <celu@infuseai.io>
2024-01-30 17:30:33 +0800

    Profile diff supports new dataframe
    Signed-off-by: popcorny <celu@infuseai.io>

--- js/src/lib/api/profile.ts
+++ js/src/lib/api/profile.ts
@@ -1,15 +1,13 @@
 import { SubmitOptions, submitRun } from "./runs";
-import { PandasDataFrame } from "./types";
+import { DataFrame } from "./types";
 
 export interface ProfileDiffParams {
   model: string;
 }
 
 export interface ProfileDiffResult {
-  base?: PandasDataFrame;
-  current?: PandasDataFrame;
-  base_error?: string;
-  current_error?: string;
+  base?: DataFrame;
+  current?: DataFrame;
 }
 
 export interface ProfileDiffViewOptions {
