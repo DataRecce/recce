d9b0a5846aff4fe9654edcac6c2d5b0e9af2feab
Kent Huang <kent@infuseai.io>
2024-02-27 11:25:19 +0800

    [Feature] Add Screshot box and fix some UI
    Signed-off-by: Kent Huang <kent@infuseai.io>

--- js/src/components/histogram/HistogramDiffResultView.tsx
+++ js/src/components/histogram/HistogramDiffResultView.tsx
@@ -1,12 +1,14 @@
 import { HistogramDiffParams, HistogramDiffResult } from "@/lib/api/profile";
 import { RunResultViewProps } from "../run/types";
-import { HStack, Box, Flex , Spacer } from "@chakra-ui/react";
+import { HStack, Box, Flex , Spacer, Heading } from "@chakra-ui/react";
 import { HistogramChart } from "../charts/HistogramChart";
+import { ScreenshotBox } from "../screenshot/ScreenshotBox";
 
 
 interface HistogramDiffResultViewProp extends RunResultViewProps<HistogramDiffParams, HistogramDiffResult> {}
 
 export function HistogramDiffResultView({ run }: HistogramDiffResultViewProp) {
+  const params = run.params as HistogramDiffParams;
   const base = run.result?.base;
   const current = run.result?.current;
   const min = run.result?.min;
@@ -17,31 +19,34 @@ export function HistogramDiffResultView({ run }: HistogramDiffResultViewProp) {
   }
 
   return (
-    <Flex direction='column' height={'100%'}>
-      <HStack>
-        <Spacer />
-        <Box w="40%" h="300px" m={4}>
-          <h2>Base</h2>
-          <HistogramChart data={{
-            type: run.params?.column_type || '',
-            histogram: base,
-            min: min,
-            max: max,
-            samples: base.total,
-          }} />
-        </Box>
-        <Box w="40%" h="300px" m={4}>
-          <h2>Current</h2>
-          <HistogramChart data={{
-            type: run.params?.column_type || '',
-            histogram: current,
-            min: min,
-            max: max,
-            samples: current.total,
-          }} />
-        </Box>
-        <Spacer />
-      </HStack>
+    <Flex direction='column' height={'80%'}>
+      <ScreenshotBox height={'100%'}>
+      <Heading as="h1" size="md" marginTop={4} textAlign='center'>Model {params.model}.{params.column_name}</Heading>
+        <HStack>
+          <Spacer />
+          <Box w="40%" h="300px" m={4}>
+            <Heading as='h3' size="sm" m="2" color='gray'>Base</Heading>
+            <HistogramChart data={{
+              type: run.params?.column_type || '',
+              histogram: base,
+              min: min,
+              max: max,
+              samples: base.total,
+            }} />
+          </Box>
+          <Box w="40%" h="300px" m={4}>
+            <Heading as='h3' size="sm" m="2" color='gray'>Current</Heading>
+            <HistogramChart data={{
+              type: run.params?.column_type || '',
+              histogram: current,
+              min: min,
+              max: max,
+              samples: current.total,
+            }} />
+          </Box>
+          <Spacer />
+        </HStack>
+      </ScreenshotBox>
     </Flex>
   );
 }
