73e84f2883781811ea9b4085fd6f184b89f309e9
Wei-Chun, Chang <wcchang@infuseai.io>
2024-03-18 18:00:55 +0800

    Fix reorder check
    Signed-off-by: Wei-Chun, Chang <wcchang@infuseai.io>

--- js/src/components/lineage/GraphNode.tsx
+++ js/src/components/lineage/GraphNode.tsx
@@ -24,10 +24,19 @@ function isSchemaChanged(
   const baseKeys = Object.keys(baseSchema);
   const currKeys = Object.keys(currSchema);
 
+  // added, removed
   if (baseKeys.length !== currKeys.length) {
     return true;
   }
 
+  // reordered
+  for (let i = 0; i < baseKeys.length; i++) {
+    if (baseKeys[i] !== currKeys[i]) {
+      return true;
+    }
+  }
+
+  // modified
   for (const key of currKeys) {
     if (!baseSchema[key] || baseSchema[key].type !== currSchema[key].type) {
       return true;
