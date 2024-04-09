31605450352725ed59a5a0f406ca23df43104a33
Wei-Chun, Chang <wcchang@infuseai.io>
2024-04-09 17:53:22 +0800

    Return none when HEAD is detached
    Signed-off-by: Wei-Chun, Chang <wcchang@infuseai.io>

--- recce/git.py
+++ recce/git.py
@@ -4,10 +4,11 @@ from git import Repo
 def current_branch():
     try:
         repo = Repo(search_parent_directories=True)
+        if not repo.active_branch:
+            return None
+        return repo.active_branch.name
+    except TypeError:
+        # happened when HEAD is a detached symbolic reference
+        return None
     except Exception:
         return None
-
-    if not repo.active_branch:
-        return None
-
-    return repo.active_branch.name
