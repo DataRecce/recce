cd267bae878f04d707b72dae134bbfb06b26772f
popcorny <celu@infuseai.io>
2024-05-02 14:52:46 +0800

    Add sqlmesh sushi
    Signed-off-by: popcorny <celu@infuseai.io>

---
+++ integration_tests/sqlmesh/models/waiter_names.sql
@@ -0,0 +1,10 @@
+MODEL (
+  name sushi.waiter_names,
+  kind SEED (
+    path '../seeds/waiter_names.csv',
+    batch_size 5
+  ),
+  owner jen,
+  grain id,
+  description 'List of waiter names'
+)
---
+++ integration_tests/sqlmesh/prep_env.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+cd "$SCRIPT_DIR"
+pwd
+
+# Step 1: prepare env
+sqlmesh --version
+sqlmesh --config local_config plan --no-prompts --auto-apply
+sqlmesh --config local_config plan dev --no-prompts --auto-apply --include-unmodified
+
+# Step 2: turn off anonymous tracking
+mkdir ~/.recce
+echo "user_id: 00000000000000000000000000000000" > ~/.recce/profile.yml
+echo "anonymous_tracking: false" >> ~/.recce/profile.yml
