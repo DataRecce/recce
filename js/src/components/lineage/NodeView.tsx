c8c3359d061b5303f1e1bf720434f51fda3f79ff
popcorny <celu@infuseai.io>
2024-09-18 16:25:14 +0800

    Make the node actions together
    Signed-off-by: popcorny <celu@infuseai.io>

--- js/src/components/lineage/NodeView.tsx
+++ js/src/components/lineage/NodeView.tsx
@@ -23,6 +23,8 @@ import {
   MenuButton,
   MenuList,
   MenuItem,
+  MenuDivider,
+  Icon,
 } from "@chakra-ui/react";
 
 import { FaCode } from "react-icons/fa";
@@ -37,11 +39,14 @@ import { useRecceActionContext } from "@/lib/hooks/RecceActionContext";
 import { useLineageGraphContext } from "@/lib/hooks/LineageGraphContext";
 import useModelColumns from "@/lib/hooks/useModelColumns";
 import { createSchemaDiffCheck } from "@/lib/api/schemacheck";
+import { findByRunType } from "../run/registry";
 
 interface NodeViewProps {
   node: LineageGraphNode;
   onCloseNode: () => void;
 }
+
+const EmptyIcon = () => <Box as="span" w="12px" />;
 
 export function NodeView({ node, onCloseNode }: NodeViewProps) {
   const [, setLocation] = useLocation();
@@ -66,6 +71,9 @@ export function NodeView({ node, onCloseNode }: NodeViewProps) {
     setLocation(`/checks/${check.check_id}`);
   }, [node, setLocation]);
 
+  const isAddedOrRemoved =
+    node.changeStatus === "added" || node.changeStatus === "removed";
+
   return (
     <Grid height="100%" templateRows="auto auto 1fr">
       <HStack>
@@ -73,32 +81,122 @@ export function NodeView({ node, onCloseNode }: NodeViewProps) {
           <Heading size="sm">{node.name}</Heading>
         </Box>
         <Spacer />
-        {node.changeStatus === "modified" && (
-          <Box>
-            <Button
-              onClick={onCodeDiffOpen}
-              leftIcon={<FaCode />}
-              colorScheme="orange"
-              variant="solid"
-            >
-              Diff
-            </Button>
-            <Modal isOpen={isCodeDiffOpen} onClose={onCodeDiffClose} size="6xl">
-              <ModalOverlay />
-              <ModalContent overflowY="auto" height="75%">
-                <ModalHeader>Model Raw Code Diff</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody>
-                  <SqlDiffView
-                    base={node.data.base}
-                    current={node.data.current}
-                  />
-                </ModalBody>
-              </ModalContent>
-            </Modal>
-          </Box>
+        {/* {node.changeStatus === "modified" && ( */}
+        {(node.resourceType === "model" || node.resourceType === "seed") && (
+          <Menu>
+            <MenuButton as={Button} size="xs" colorScheme="blue">
+              Explore Changes
+            </MenuButton>
+            <MenuList>
+              <MenuItem
+                icon={<Icon as={findByRunType("query_diff")?.icon} />}
+                fontSize="14px"
+                onClick={() => {
+                  if (envInfo?.adapterType === "dbt") {
+                    setSqlQuery(`select * from {{ ref("${node.name}") }}`);
+                  } else if (envInfo?.adapterType === "sqlmesh") {
+                    setSqlQuery(`select * from ${node.name}`);
+                  }
+                  setPrimaryKeys(
+                    primaryKey !== undefined ? [primaryKey] : undefined
+                  );
+                  setLocation("/query");
+                }}
+              >
+                Query
+              </MenuItem>
+              <MenuDivider />
+              <MenuItem
+                onClick={onCodeDiffOpen}
+                icon={<FaCode />}
+                fontSize="14px"
+              >
+                Code Diff
+              </MenuItem>
+              <MenuItem
+                icon={<Icon as={findByRunType("row_count_diff")?.icon} />}
+                fontSize="14px"
+                onClick={() => {}}
+              >
+                Row Count Diff
+              </MenuItem>
+              <MenuItem
+                icon={<Icon as={findByRunType("profile_diff")?.icon} />}
+                fontSize="14px"
+                isDisabled={isAddedOrRemoved}
+                onClick={() => {
+                  runAction(
+                    "profile_diff",
+                    {
+                      model: node.name,
+                    },
+                    { showForm: false, showLast: true }
+                  );
+                }}
+              >
+                Profile Diff
+              </MenuItem>
+              <MenuItem
+                icon={<Icon as={findByRunType("value_diff")?.icon} />}
+                fontSize="14px"
+                isDisabled={isAddedOrRemoved}
+                onClick={() => {
+                  runAction(
+                    "value_diff",
+                    {
+                      model: node.name,
+                    },
+                    { showForm: true, showLast: true }
+                  );
+                }}
+              >
+                Value Diff
+              </MenuItem>
+              <MenuItem
+                icon={<Icon as={findByRunType("top_k_diff")?.icon} />}
+                fontSize="14px"
+                isDisabled={isAddedOrRemoved}
+                onClick={() => {
+                  runAction(
+                    "top_k_diff",
+                    { model: node.name, column_name: "", k: 50 },
+                    { showForm: true }
+                  );
+                }}
+              >
+                Top-K Diff
+              </MenuItem>
+              <MenuItem
+                icon={<Icon as={findByRunType("histogram_diff")?.icon} />}
+                fontSize="14px"
+                isDisabled={isAddedOrRemoved}
+                onClick={() => {
+                  runAction(
+                    "histogram_diff",
+                    {
+                      model: node.name,
+                      column_name: "",
+                      column_type: "",
+                    },
+                    { showForm: true }
+                  );
+                }}
+              >
+                Histogram Diff
+              </MenuItem>
+              <MenuDivider />
+              <MenuItem
+                icon={<Icon as={findByRunType("schema_diff")?.icon} />}
+                fontSize="14px"
+                onClick={addSchemaCheck}
+              >
+                Schema Diff Check
+              </MenuItem>
+            </MenuList>
+          </Menu>
         )}
-        <Box flex="0 1 1%" p="16px">
+        {/* )} */}
+        <Box flex="0 1 1%">
           <CloseButton onClick={onCloseNode} />
         </Box>
       </HStack>
@@ -112,116 +210,27 @@ export function NodeView({ node, onCloseNode }: NodeViewProps) {
         </HStack>
       </Box>
       {withColumns && (
-        <>
-          <Tabs overflow="auto" as={Flex}>
-            <TabList>
-              <Tab>Columns</Tab>
-            </TabList>
-            <TabPanels overflow="auto" height="calc(100% - 42px)">
-              <TabPanel p={0} overflowY="auto" height="100%">
-                <SchemaView base={node.data.base} current={node.data.current} />
-              </TabPanel>
-            </TabPanels>
-          </Tabs>
-          <HStack p="16px">
-            <Menu>
-              <MenuButton as={Button} size="sm" colorScheme="blue">
-                Add check
-              </MenuButton>
-              <MenuList>
-                <MenuItem onClick={addSchemaCheck}>Schema Check</MenuItem>
-              </MenuList>
-            </Menu>
-            <Spacer />
-
-            {node.resourceType === "model" && (
-              <>
-                {node.changeStatus !== "added" &&
-                  node.changeStatus !== "removed" && (
-                    <>
-                      <Menu>
-                        <MenuButton as={Button} size="sm" colorScheme="blue">
-                          Advanced Diffs
-                        </MenuButton>
-                        <MenuList>
-                          <MenuItem
-                            onClick={() => {
-                              runAction(
-                                "profile_diff",
-                                {
-                                  model: node.name,
-                                },
-                                { showForm: false, showLast: true }
-                              );
-                            }}
-                          >
-                            Profile Diff
-                          </MenuItem>
-                          <MenuItem
-                            onClick={() => {
-                              runAction(
-                                "value_diff",
-                                {
-                                  model: node.name,
-                                },
-                                { showForm: true, showLast: true }
-                              );
-                            }}
-                          >
-                            Value Diff
-                          </MenuItem>
-                          <MenuItem
-                            onClick={() => {
-                              runAction(
-                                "top_k_diff",
-                                { model: node.name, column_name: "", k: 50 },
-                                { showForm: true }
-                              );
-                            }}
-                          >
-                            Top-K Diff
-                          </MenuItem>
-                          <MenuItem
-                            onClick={() => {
-                              runAction(
-                                "histogram_diff",
-                                {
-                                  model: node.name,
-                                  column_name: "",
-                                  column_type: "",
-                                },
-                                { showForm: true }
-                              );
-                            }}
-                          >
-                            Histogram Diff
-                          </MenuItem>
-                        </MenuList>
-                      </Menu>
-                    </>
-                  )}
-                <Button
-                  colorScheme="blue"
-                  size="sm"
-                  onClick={() => {
-                    if (envInfo?.adapterType === "dbt") {
-                      setSqlQuery(`select * from {{ ref("${node.name}") }}`);
-                    } else if (envInfo?.adapterType === "sqlmesh") {
-                      setSqlQuery(`select * from ${node.name}`);
-                    }
-                    setPrimaryKeys(
-                      primaryKey !== undefined ? [primaryKey] : undefined
-                    );
-                    setLocation("/query");
-                  }}
-                >
-                  Query
-                </Button>
-              </>
-            )}
-          </HStack>
-        </>
+        <Tabs overflow="auto" as={Flex}>
+          <TabList>
+            <Tab>Columns</Tab>
+          </TabList>
+          <TabPanels overflow="auto" height="calc(100% - 42px)">
+            <TabPanel p={0} overflowY="auto" height="100%">
+              <SchemaView base={node.data.base} current={node.data.current} />
+            </TabPanel>
+          </TabPanels>
+        </Tabs>
       )}
+      <Modal isOpen={isCodeDiffOpen} onClose={onCodeDiffClose} size="6xl">
+        <ModalOverlay />
+        <ModalContent overflowY="auto" height="75%">
+          <ModalHeader>Model Raw Code Diff</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <SqlDiffView base={node.data.base} current={node.data.current} />
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Grid>
   );
 }
