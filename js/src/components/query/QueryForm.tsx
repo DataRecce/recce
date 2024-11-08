66d88b4f724f14bdb0e19d6f5ddcf897e9ce3791
Kent Huang <kent@infuseai.io>
2024-11-08 18:22:41 +0800

    [Refine] refactor PrimaryKey input text field by DropdownValuesInput
    Signed-off-by: Kent Huang <kent@infuseai.io>

--- js/src/components/query/QueryForm.tsx
+++ js/src/components/query/QueryForm.tsx
@@ -1,22 +1,11 @@
 import { useLineageGraphContext } from "@/lib/hooks/LineageGraphContext";
-import { TagInput } from "@/utils/TagInput";
+import { DropdownValuesInput } from "@/utils/DropdownValuesInput";
 import { InfoIcon } from "@chakra-ui/icons";
 import {
-  Button,
   Flex,
   FlexProps,
   FormControl,
   FormLabel,
-  Input,
-  InputGroup,
-  InputRightElement,
-  Menu,
-  MenuButton,
-  MenuDivider,
-  MenuGroup,
-  MenuItem,
-  MenuList,
-  Portal,
   Tooltip,
 } from "@chakra-ui/react";
 import { useMemo, useRef, useState } from "react";
@@ -26,19 +15,16 @@ interface QueryFormProps extends FlexProps {
   onPrimaryKeysChange: (primaryKeys: string[]) => void;
 }
 
-const PrimaryKeySelectMenu = ({
+export const QueryForm = ({
   defaultPrimaryKeys,
   onPrimaryKeysChange,
   ...prob
 }: QueryFormProps) => {
   const { lineageGraph } = useLineageGraphContext();
-  const [filter, setFilter] = useState("");
-  const [selectedKeys, setSelectedKeys] = useState<string[]>(
-    defaultPrimaryKeys || []
-  );
-  const inputRef = useRef();
+  const labelInfo =
+    "Provide a primary key to perform query diff in data warehouse and only return changed rows.";
 
-  const columns = useMemo(() => {
+  const availableColumns = useMemo(() => {
     if (!lineageGraph) {
       return [];
     }
@@ -59,117 +45,6 @@ const PrimaryKeySelectMenu = ({
     return Array.from(columnSet).sort();
   }, [lineageGraph]);
 
-  const showNumberOfKeysSelected = (keys: string[]) => {
-    if (keys.length > 1) {
-      return `${keys.length} keys selected`;
-    } else if (keys.length === 1) {
-      return `${keys.length} key selected`;
-    }
-    return "";
-  };
-
-  const handleSelect = (column: string) => {
-    if (!selectedKeys.includes(column)) {
-      setFilter("");
-      setSelectedKeys([...selectedKeys, column]);
-      onPrimaryKeysChange([...selectedKeys, column]);
-    }
-  };
-
-  const handleClear = () => {
-    setFilter("");
-    setSelectedKeys([]);
-    onPrimaryKeysChange([]);
-  };
-
-  return (
-    <InputGroup size="xs" width={"240px"}>
-      <Menu isLazy closeOnSelect={false}>
-        <MenuButton width={"100%"}>
-          <Input
-            placeholder="Start by typing key name..."
-            size="xs"
-            borderRadius={"4px"}
-            value={showNumberOfKeysSelected(selectedKeys)}
-          />
-        </MenuButton>
-        <Portal>
-          <MenuList zIndex={"dropdown"} fontSize={"xs"} width={"240px"}>
-            {/* Filter  */}
-            <MenuGroup>
-              <TagInput
-                ref={inputRef}
-                placeholder="Filter keys or add custom"
-                value={filter}
-                onValueChange={(val) => setFilter(val)}
-                tags={selectedKeys}
-                onTagChange={(tag, action) => {
-                  if (action === "add" && tag) {
-                    setSelectedKeys([...selectedKeys, tag]);
-                    onPrimaryKeysChange([...selectedKeys, tag]);
-                  } else {
-                    setSelectedKeys(selectedKeys.filter((key) => key !== tag));
-                    onPrimaryKeysChange(
-                      selectedKeys.filter((key) => key !== tag)
-                    );
-                  }
-                }}
-                size="xs"
-              />
-            </MenuGroup>
-            <MenuDivider />
-            {/* Columns */}
-            <MenuGroup>
-              {filter !== "" && !columns.includes(filter) && (
-                // <MenuGroup>
-                <MenuItem
-                  key={"custom-key-by-filter"}
-                  onClick={() => handleSelect(filter)}
-                >
-                  Add &apos;{filter}&apos; to the list
-                </MenuItem>
-                // </MenuGroup>
-              )}
-              {columns
-                .filter((column) => filter === "" || column.includes(filter))
-                .filter((column) => !selectedKeys.includes(column))
-                .map((column, cid) => (
-                  <MenuItem
-                    key={`option-${cid}`}
-                    onClick={() => handleSelect(column)}
-                  >
-                    {column}
-                  </MenuItem>
-                ))}
-            </MenuGroup>
-          </MenuList>
-        </Portal>
-      </Menu>
-      <InputRightElement>
-        <Button
-          variant={"link"}
-          color={"#3182CE"}
-          fontSize={"xs"}
-          paddingTop="4px"
-          paddingRight={"24px"}
-          onClick={handleClear}
-          hidden={selectedKeys.length === 0}
-        >
-          Clear
-        </Button>
-      </InputRightElement>
-    </InputGroup>
-  );
-};
-
-export const QueryForm = ({
-  defaultPrimaryKeys,
-  onPrimaryKeysChange,
-  ...prob
-}: QueryFormProps) => {
-  const labelInfo =
-    "Provide a primary key to perform query diff in data warehouse and only return changed rows.";
-
   return (
     <Flex {...prob}>
       <FormControl m="4px 8px">
@@ -179,9 +54,14 @@ export const QueryForm = ({
             <InfoIcon color="gray.600" boxSize="3" />
           </Tooltip>
         </FormLabel>
-        <PrimaryKeySelectMenu
-          defaultPrimaryKeys={defaultPrimaryKeys}
-          onPrimaryKeysChange={onPrimaryKeysChange}
+        <DropdownValuesInput
+          unitName="key"
+          defaultValues={defaultPrimaryKeys}
+          suggestionList={availableColumns}
+          onValuesChange={onPrimaryKeysChange}
+          size="xs"
+          width={"240px"}
+          placeholder="Start by typing key name..."
         />
       </FormControl>
     </Flex>
---
+++ js/src/utils/DropdownValuesInput.tsx
@@ -0,0 +1,184 @@
+import React, { useState, useRef } from "react";
+import {
+  Button,
+  Input,
+  InputGroup,
+  InputProps,
+  InputRightElement,
+  Menu,
+  MenuButton,
+  MenuDivider,
+  MenuGroup,
+  MenuItem,
+  MenuList,
+  Portal,
+  Tag,
+  TagCloseButton,
+  TagLabel,
+  Wrap,
+  WrapItem,
+} from "@chakra-ui/react";
+
+export interface DropdownValuesInputProps extends InputProps {
+  unitName: string;
+  suggestionList?: string[];
+  defaultValues?: string[];
+  onValuesChange: (values: string[]) => void;
+}
+
+export const DropdownValuesInput = (props: DropdownValuesInputProps) => {
+  const { defaultValues, suggestionList, onValuesChange } = props;
+  const [values, setValues] = useState<string[]>(defaultValues || []);
+  const [filter, setFilter] = useState<string>("");
+  const [isTyping, setIsTyping] = useState<boolean>(false);
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const showNumberOfValuesSelected = (tags: string[]) => {
+    if (tags.length > 1) {
+      return `${tags.length} ${props.unitName}s selected`;
+    } else if (tags.length === 1) {
+      return `${tags.length} ${props.unitName} selected`;
+    }
+    return "";
+  };
+
+  const handleSelect = (value: string) => {
+    if (!values.includes(value)) {
+      setFilter("");
+      setValues([...values, value]);
+      onValuesChange([...values, value]);
+    }
+  };
+
+  const handleClear = () => {
+    setFilter("");
+    setValues([]);
+    onValuesChange([]);
+  };
+
+  return (
+    <InputGroup size={props.size} width={props.width}>
+      <Menu isLazy closeOnSelect={false}>
+        <MenuButton width={"100%"}>
+          <Input
+            placeholder={props.placeholder}
+            size={props.size}
+            borderRadius={"4px"}
+            value={showNumberOfValuesSelected(values)}
+          />
+        </MenuButton>
+        <Portal>
+          <MenuList
+            zIndex={"dropdown"}
+            fontSize={props.size}
+            width={props.width}
+          >
+            {/* Input Filter & Show Tags */}
+            <MenuGroup>
+              <Wrap
+                border={"1px solid #e2e8f0"}
+                borderRadius={"4px"}
+                width={"calc(100% - 8px)"}
+                marginX={"4px"}
+                padding={"4px"}
+              >
+                {values.map((value, cid) => (
+                  <WrapItem key={`tag-${cid}`}>
+                    <Tag key={value} size={props.size}>
+                      <TagLabel paddingLeft={"8px"}>{value}</TagLabel>
+                      <TagCloseButton
+                        paddingRight={"8px"}
+                        onClick={() => {
+                          setValues(values.filter((v) => v !== value));
+                          onValuesChange(values.filter((v) => v !== value));
+                        }}
+                      />
+                    </Tag>
+                  </WrapItem>
+                ))}
+                <WrapItem width={"100%"}>
+                  <Input
+                    ref={inputRef}
+                    placeholder="Filter keys or add custom"
+                    variant={"unstyled"}
+                    size={props.size}
+                    value={filter}
+                    onChange={(e) => {
+                      setFilter(e.target.value);
+                      setIsTyping(true);
+                    }}
+                    onKeyDown={(e) => {
+                      const newText = e.currentTarget.value
+                        .trim()
+                        .replace(",", "");
+                      switch (e.key) {
+                        case ",":
+                        case "Enter":
+                          handleSelect(newText);
+                          setFilter("");
+                          break;
+                        case "Backspace":
+                          if (
+                            e.currentTarget.value === "" &&
+                            values.length > 0
+                          ) {
+                            setValues(values.slice(0, -1));
+                            onValuesChange(values.slice(0, -1));
+                          }
+                          break;
+                        default:
+                          break;
+                      }
+                    }}
+                    onBlur={() => {
+                      if (inputRef && isTyping) inputRef?.current?.focus();
+                    }}
+                  />
+                </WrapItem>
+              </Wrap>
+            </MenuGroup>
+            <MenuDivider />
+            {/* Suggestion List */}
+            <MenuGroup>
+              {filter !== "" && !suggestionList?.includes(filter) && (
+                <MenuItem
+                  key={"custom-value-by-filter"}
+                  onClick={() => {
+                    handleSelect(filter);
+                    setIsTyping(false);
+                  }}
+                >
+                  Add &apos;{filter}&apos; to the list
+                </MenuItem>
+              )}
+              {suggestionList
+                ?.filter((value) => filter === "" || value.includes(filter))
+                .filter((value) => !values.includes(value))
+                ?.map((value, cid) => (
+                  <MenuItem
+                    key={`option-${cid}`}
+                    onClick={() => handleSelect(value)}
+                  >
+                    {value}
+                  </MenuItem>
+                ))}
+            </MenuGroup>
+          </MenuList>
+        </Portal>
+      </Menu>
+      <InputRightElement>
+        <Button
+          variant={"link"}
+          color={"#3182CE"}
+          fontSize={props.size}
+          paddingTop="4px"
+          paddingRight={"24px"}
+          hidden={values.length === 0}
+          onClick={handleClear}
+        >
+          Clear
+        </Button>
+      </InputRightElement>
+    </InputGroup>
+  );
+};
