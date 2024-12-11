a2d9cd0fa5ef11e8fe6810e2f5de600ca1d85c62
Kent Huang <kent@infuseai.io>
2024-12-11 15:12:17 +0800

    [Fix] typo
    Signed-off-by: Kent Huang <kent@infuseai.io>

--- js/src/constants/tooltipMessage.ts
+++ js/src/constants/tooltipMessage.ts
@@ -1,7 +1,7 @@
 export const DisableTooltipMessages = {
   audit_helper:
-    "Require the dbt package 'audit_helper'. Please install it and re-run dbt to activate this feature.",
-  dbt_profile:
-    "Require the dbt package 'dbt_profile'. Please install it and re-run dbt to activate this feature.",
+    "Require the dbt package 'dbt-labs/audit_helper'. Please install it and re-run dbt to activate this feature.",
+  dbt_profiler:
+    "Require the dbt package 'data-mie/dbt_profiler'. Please install it and re-run dbt to activate this feature.",
   add_or_remove: "Unavailable for added or removed resources.",
 } as const;
