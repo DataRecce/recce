5d77399e091f441936aa02898727e8e94d31ec94
Kent Huang <kent@infuseai.io>
2024-12-11 14:12:17 +0800

    [Add] new file for tooltip message
    Signed-off-by: Kent Huang <kent@infuseai.io>

---
+++ js/src/constants/tooltipMessage.ts
@@ -0,0 +1,7 @@
+export const DisableTooltipMessages = {
+  audit_helper:
+    "Require the dbt package 'audit_helper'. Please install it and re-run dbt to activate this feature.",
+  dbt_profile:
+    "Require the dbt package 'dbt_profile'. Please install it and re-run dbt to activate this feature.",
+  add_or_remove: "Unavailable for added or removed resources.",
+} as const;
