996aec14430fb3094044f9523e69a6db4e81b2c3
Kent Huang <kent@infuseai.io>
2024-12-13 16:48:56 +0800

    [Test] Add test case for schema_diff_should_be_approved
    Signed-off-by: Kent Huang <kent@infuseai.io>

--- tests/tasks/test_schema.py
+++ tests/tasks/test_schema.py
@@ -1,4 +1,12 @@
+import os
+from unittest import TestCase
+from unittest.mock import MagicMock
+
 import pytest
+
+from recce.adapter.dbt_adapter import load_manifest, load_catalog, DbtAdapter
+from recce.core import RecceContext, set_default_context
+from recce.run import schema_diff_should_be_approved
 
 
 def test_validator():
@@ -48,3 +56,41 @@ def test_validator():
         validate({
             'view_mode': 'abc',
         })
+
+
+test_root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+
+class TestSchemaDiffAutoApprove(TestCase):
+
+    def setUp(self):
+        self.default_context = MagicMock(spec=RecceContext)
+        manifest = load_manifest(path=os.path.join(test_root_path, 'manifest.json'))
+        catalog = load_catalog(path=os.path.join(test_root_path, 'catalog.json'))
+        dbt_adapter = DbtAdapter(curr_manifest=manifest, curr_catalog=catalog)
+        self.default_context.adapter = dbt_adapter
+
+        dbt_adapter.select_nodes = MagicMock()
+        # Base and Current will be the same
+        self.default_context.get_lineage.return_value = dbt_adapter.get_lineage()
+        set_default_context(self.default_context)
+
+    def test_schema_diff_should_be_approved(self):
+        # Node_id is string
+        is_approved = schema_diff_should_be_approved({
+            'node_id': 'model.jaffle_shop.customers',
+        })
+        assert is_approved is True
+
+        # Node_id is list
+        is_approved = schema_diff_should_be_approved({
+            'node_id': ['model.jaffle_shop.customers'],
+        })
+        assert is_approved is True
+
+        # Select all models
+        self.default_context.adapter.select_nodes.return_value = ['model.jaffle_shop.customers']
+        is_approved = schema_diff_should_be_approved({
+            'select': 'customers',
+        })
+        assert is_approved is True
