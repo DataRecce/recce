e190171c9d0a20db83f867474a6f6dd3d752d2ed
Kent Huang <kent@infuseai.io>
2024-12-23 11:09:12 +0800

    [Feature] DRC-972 add feedback collection toast
    Signed-off-by: Kent Huang <kent@infuseai.io>

--- js/src/lib/api/localStorageKeys.ts
+++ js/src/lib/api/localStorageKeys.ts
@@ -2,4 +2,5 @@ const prefix = "recce-";
 
 export const localStorageKeys = {
   bypassSaveOverwrite: `${prefix}-bypass-save-overwrite`,
+  bypassPreviewChangeFeedback: `${prefix}-preview-change-feedback`,
 };
