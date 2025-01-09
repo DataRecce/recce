1abc27f879ba08bc5fddf27d842e12793a091a2a
popcorny <celu@infuseai.io>
2025-01-09 18:04:16 +0800

    Support breaking change analysis
    Signed-off-by: popcorny <celu@infuseai.io>

--- js/src/components/lineage/GraphNode.tsx
+++ js/src/components/lineage/GraphNode.tsx
@@ -141,12 +141,13 @@ const GraphNodeCheckbox = ({
 };
 
 export function GraphNode({ data }: GraphNodeProps) {
-  const { isHighlighted, isSelected, resourceType, changeStatus } = data;
+  const { id, isHighlighted, isSelected, resourceType, changeStatus } = data;
   const showContent = useStore((s) => s.transform[2] > 0.3);
 
   const { icon: resourceIcon } = getIconForResourceType(resourceType);
   const [isHovered, setIsHovered] = useState(false);
   const { interactive, selectNodeMulti, selectMode } = useLineageViewContext();
+  const { lineageGraph } = useLineageGraphContext();
 
   // text color, icon
   const {
@@ -217,7 +218,7 @@ export function GraphNode({ data }: GraphNodeProps) {
         <Flex
           // backgroundColor={color}
           bg={
-            changeStatus === "modified"
+            changeStatus === "modified" && lineageGraph?.nonBreakingSet.has(id)
               ? "repeating-linear-gradient(45deg, orange, orange 5px, white 5px, white 10px)"
               : color
           }
