5d782dc87df0d834e4b50e88ab72c3bd61c6ef0c
popcorny <celu@infuseai.io>
2025-01-15 09:08:42 +0800

    Add breaking change analysis
    Signed-off-by: popcorny <celu@infuseai.io>

--- recce/util/breaking.py
+++ recce/util/breaking.py
@@ -1,3 +1,6 @@
+from sqlglot.optimizer import optimize
+
+
 def is_breaking_change(original_sql, modified_sql, dialect=None):
     if original_sql == modified_sql:
         return False
@@ -10,15 +13,15 @@ def is_breaking_change(original_sql, modified_sql, dialect=None):
         return True
 
     try:
-        original_ast = parse_one(original_sql, dialect=dialect)
-        modified_ast = parse_one(modified_sql, dialect=dialect)
+        original_ast = optimize(parse_one(original_sql, dialect=dialect))
+        modified_ast = optimize(parse_one(modified_sql, dialect=dialect))
     except Exception:
         return True
 
     if not isinstance(original_ast, exp.Select) or not isinstance(modified_ast, exp.Select):
         raise ValueError("Currently only SELECT statements are supported for comparison")
 
-    edits = diff(original_ast, modified_ast)
+    edits = diff(original_ast, modified_ast, delta_only=True)
 
     inserted_expressions = {
         e.expression for e in edits if isinstance(e, Insert)
@@ -28,7 +31,24 @@ def is_breaking_change(original_sql, modified_sql, dialect=None):
         if isinstance(edit, Insert):
             inserted_expr = edit.expression
 
-            if isinstance(inserted_expr, exp.Where):
+            if (
+                isinstance(inserted_expr, exp.Where) or
+                isinstance(inserted_expr, exp.Join) or
+                isinstance(inserted_expr, exp.Order) or
+                isinstance(inserted_expr, exp.Group) or
+                isinstance(inserted_expr, exp.Having) or
+                isinstance(inserted_expr, exp.Limit) or
+                isinstance(inserted_expr, exp.Offset) or
+                isinstance(inserted_expr, exp.Window) or
+                isinstance(inserted_expr, exp.Union) or
+                isinstance(inserted_expr, exp.Intersect) or
+                isinstance(inserted_expr, exp.Except) or
+                isinstance(inserted_expr, exp.Merge) or
+                isinstance(inserted_expr, exp.Delete) or
+                isinstance(inserted_expr, exp.Update) or
+                isinstance(inserted_expr, exp.Insert) or
+                isinstance(inserted_expr, exp.Subquery)
+            ):
                 return True
 
             if (
