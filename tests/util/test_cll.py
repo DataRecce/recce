440cea269627c9de194eed8670beaff2592e676a
popcorny <celu@infuseai.io>
2025-02-17 13:56:08 +0800

    Add transformation type
    Signed-off-by: popcorny <celu@infuseai.io>

--- tests/util/test_cll.py
+++ tests/util/test_cll.py
@@ -1,6 +1,6 @@
 import unittest
 from dataclasses import dataclass
-from typing import Dict
+from typing import Dict, List, Literal
 
 from sqlglot import parse_one
 from sqlglot.expressions import Column, Alias, Func, Binary, Paren, Case, Expression
@@ -9,63 +9,73 @@ from sqlglot.optimizer.qualify import qualify
 
 
 @dataclass
-class ColumnLevelDependencyColumn:
-    type: str
+class ColumnLevelDependsOn:
+    node: str
+    column: str
 
 
 @dataclass
-class ColumnLevelDependencyModel:
-    columns: Dict[str, ColumnLevelDependencyColumn]
+class ColumnLevelDependencyColumn:
+    type: Literal['source', 'passthrough', 'renamed', 'derived']
+    depends_on: List[ColumnLevelDependsOn]
 
 
-ColumnLevelDependency = Dict[str, ColumnLevelDependencyModel]
-
-
-def _cll_expression(expression, default_table):
+def _cll_expression(expression, default_table) -> ColumnLevelDependencyColumn:
     # given an expression, return the columns depends on
     # [{node: table, column: column}, ...]
 
     if isinstance(expression, Column):
         column = expression
-        return [{"node": column.table or default_table, "column": column.name}]
+        return ColumnLevelDependencyColumn(
+            type='passthrough',
+            depends_on=[ColumnLevelDependsOn(column.table or default_table, column.name)]
+        )
     elif isinstance(expression, Paren):
         return _cll_expression(expression.this, default_table)
     elif isinstance(expression, Binary):
         depends_on = []
         if expression.left:
-            depends_on_left = _cll_expression(expression.left, default_table)
+            depends_on_left = _cll_expression(expression.left, default_table).depends_on
             depends_on.extend(depends_on_left)
         if expression.right:
-            depends_on_right = _cll_expression(expression.right, default_table)
+            depends_on_right = _cll_expression(expression.right, default_table).depends_on
             depends_on.extend(depends_on_right)
-        return depends_on
+        type = 'derived' if depends_on else 'source'
+        return ColumnLevelDependencyColumn(type=type, depends_on=depends_on)
     elif isinstance(expression, Case):
         ifs = expression.args['ifs']
         default = expression.args['default']
         depends_on = []
         for expr in ifs:
-            depends_on_one = _cll_expression(expr, default_table)
+            depends_on_one = _cll_expression(expr, default_table).depends_on
             depends_on.extend(depends_on_one)
-        depends_on.extend(_cll_expression(default, default_table))
-        return depends_on
-
+        depends_on.extend(_cll_expression(default, default_table).depends_on)
+        type = 'derived' if depends_on else 'source'
+        return ColumnLevelDependencyColumn(type=type, depends_on=depends_on)
     elif isinstance(expression, Func):
         if expression.expressions:
             depends_on = []
             for expr in expression.expressions:
-                depends_on_one = _cll_expression(expr, default_table)
+                depends_on_one = _cll_expression(expr, default_table).depends_on
                 depends_on.extend(depends_on_one)
-            return depends_on
+            type = 'derived' if depends_on else 'source'
+            return ColumnLevelDependencyColumn(type=type, depends_on=depends_on)
         if expression.this:
-            return _cll_expression(expression.this, default_table)
+            depends_on = _cll_expression(expression.this, default_table).depends_on
+            type = 'derived' if depends_on else 'source'
+            return ColumnLevelDependencyColumn(type=type, depends_on=depends_on)
+
         return []
     elif expression.this and isinstance(expression.this, Expression):
-        return _cll_expression(expression.this, default_table)
+        depends_on = _cll_expression(expression.this, default_table).depends_on
+        type = 'derived' if depends_on else 'source'
+        return ColumnLevelDependencyColumn(type=type, depends_on=depends_on)
     else:
-        return []
+        depends_on = []
+        return ColumnLevelDependencyColumn(type='source', depends_on=depends_on)
 
 
-def cll(sql, schema=None):
+def cll(sql, schema=None) -> Dict[str, ColumnLevelDependencyColumn]:
     # given a sql, return the columns depends on
     # {
     #   'column1': {
@@ -92,19 +102,29 @@ def cll(sql, schema=None):
 
         for select in scope.expression.selects:
             # instance of Column
-            col_depends_on = []
             if isinstance(select, Column):
+                # 'select a'
                 column = select
-                col_depends_on = _cll_expression(column, default_table)
+                column_cll = _cll_expression(column, default_table)
             elif isinstance(select, Alias):
+                # 'select a as b'
+                # 'select CURRENT_TIMESTAMP() as create_at'
                 alias = select
                 col_expression = alias.this
-                col_depends_on = _cll_expression(col_expression, default_table)
+                column_cll = _cll_expression(col_expression, default_table)
+                if (column_cll and
+                    column_cll.type == 'passthrough' and
+                    column_cll.depends_on[0].column != alias.alias_or_name
+                ):
+                    column_cll.type = 'renamed'
+            else:
+                # 'select 1'
+                column_cll = ColumnLevelDependencyColumn(type='source', depends_on=[])
 
             flatten_col_depends_on = []
-            for col_dep in col_depends_on:
-                col_dep_node = col_dep['node']
-                col_dep_column = col_dep['column']
+            for col_dep in column_cll.depends_on:
+                col_dep_node = col_dep.node
+                col_dep_column = col_dep.column
                 cte_scope = scope.cte_sources.get(col_dep_node)
                 if cte_scope is not None:
                     cte_cll = global_lineage[cte_scope]
@@ -116,23 +136,20 @@ def cll(sql, schema=None):
             dedup_col_depends_on = []
             dedup_set = set()
             for col_dep in flatten_col_depends_on:
-                node_col = col_dep['node'] + '.' + col_dep['column']
+                node_col = col_dep.node + '.' + col_dep.column
                 if node_col not in dedup_set:
                     dedup_col_depends_on.append(col_dep)
                     dedup_set.add(node_col)
 
-            scope_lineage[select.alias_or_name] = dedup_col_depends_on
+            scope_lineage[select.alias_or_name] = ColumnLevelDependencyColumn(
+                type=column_cll.type,
+                depends_on=dedup_col_depends_on
+            )
 
         global_lineage[scope] = scope_lineage
         if not scope.is_cte:
             result = scope_lineage
 
-    # 第二個 pass: 解析全域層級的 dependency map，確保 external table 的 dependency
-    # for column, depends_on in scope_lineage.items():
-    #     global_lineage[column] = {
-    #         "type": "transform" if len(depends_on) > 1 else "original",
-    #         "depends_on": depends_on,
-    #     }
     return result
 
 
@@ -142,51 +159,58 @@ class ColumnLevelLineageTest(unittest.TestCase):
         select a,b from table1
         """
         result = cll(sql)
-        assert (result['a'][0]['node'] == 'table1')
-        assert (result['b'][0]['node'] == 'table1')
+        assert result['a'].type == 'passthrough'
+        assert result['a'].depends_on[0].node == 'table1'
 
     def test_sql_with_canonial(self):
         sql = """
         select table1.a from schema1.table1
         """
         result = cll(sql)
-        assert (result['a'][0]['node'] == 'table1')
+        assert result['a'].type == 'passthrough'
+        assert result['a'].depends_on[0].node == 'table1'
 
         sql = """
         select table1.a from db1.schema1.table1
         """
         result = cll(sql)
-        assert (result['a'][0]['node'] == 'table1')
+        assert result['a'].type == 'passthrough'
+        assert result['a'].depends_on[0].node == 'table1'
 
     def test_select_star(self):
         sql = """
         select * from table1
         """
         result = cll(sql, {'table1': {'a': 'int', 'b': 'int'}})
-        assert (result['a'][0]['node'] == 'table1')
-        assert (result['b'][0]['node'] == 'table1')
+        assert result['a'].type == 'passthrough'
+        assert result['a'].depends_on[0].node == 'table1'
+        assert result['b'].type == 'passthrough'
+        assert result['b'].depends_on[0].node == 'table1'
 
     def test_source_literal(self):
-        # numeric literal
-        sql = """
-        select 1
-        """
-        result = cll(sql)
-        assert len(result['1']) == 0
-
-        # string literal
-        sql = """
-        select 'abc' as c
-        """
-        result = cll(sql)
-        assert len(result['c']) == 0
+        # # numeric literal
+        # sql = """
+        # select 1
+        # """
+        # result = cll(sql)
+        # assert result['1'].type == 'source'
+        # assert len(result['1'].depends_on) == 0
+        #
+        # # string literal
+        # sql = """
+        # select 'abc' as c
+        # """
+        # result = cll(sql)
+        # assert result['c'].type == 'source'
+        # assert len(result['c'].depends_on) == 0
 
         # timestamp literal
         sql = """
         select timestamp '2021-01-01 00:00:00' as c
         """
         result = cll(sql)
-        assert len(result['c']) == 0
+        assert result['c'].type == 'source'
+        assert len(result['c'].depends_on) == 0
 
     def test_source_generative_function(self):
         # numeric literal
@@ -208,8 +232,9 @@ class ColumnLevelLineageTest(unittest.TestCase):
         select a as c from table1
         """
         result = cll(sql)
-        assert result['c'][0]['node'] == 'table1'
-        assert result['c'][0]['column'] == 'a'
+        assert result['c'].type == 'renamed'
+        assert result['c'].depends_on[0].node == 'table1'
+        assert result['c'].depends_on[0].column == 'a'
 
     def test_forward_ref(self):
         sql = """
@@ -219,10 +244,12 @@ class ColumnLevelLineageTest(unittest.TestCase):
         from table1
         """
         result = cll(sql)
-        assert result['b'][0]['node'] == 'table1'
-        assert result['b'][0]['column'] == 'a'
-        assert result['c'][0]['node'] == 'table1'
-        assert result['c'][0]['column'] == 'a'
+        assert result['b'].type == 'renamed'
+        assert result['b'].depends_on[0].node == 'table1'
+        assert result['b'].depends_on[0].column == 'a'
+        assert result['c'].type == 'renamed'
+        assert result['c'].depends_on[0].node == 'table1'
+        assert result['c'].depends_on[0].column == 'a'
 
     def test_transform_case_when(self):
         sql = """
@@ -233,9 +260,10 @@ class ColumnLevelLineageTest(unittest.TestCase):
         end as x from table1
         """
         result = cll(sql)
-        assert result['x'][0]['node'] == 'table1'
-        assert result['x'][0]['column'] == 'a'
-        assert len(result['x']) == 1
+        assert result['x'].type == 'derived'
+        assert result['x'].depends_on[0].node == 'table1'
+        assert result['x'].depends_on[0].column == 'a'
+        assert len(result['x'].depends_on) == 1
 
     def test_transform_binary(self):
         sql = """
