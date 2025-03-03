4dfa7dc0a9edb375e43d93cb88cc76eebf77dca0
Jared Scott <jared.scott@variable.team>
2025-03-03 13:44:13 +0800

    chore: Update eslint to version 9
    - Update eslint to latest version 9.
    - Remove .prettierrc and .eslintrc.json files and replace with eslint.config.mjs.
    - Include a recommended list of lint options.
    - Set error-generating configurations to warn for now
    
    Signed-off-by: Jared Scott <jared.scott@variable.team>

---
+++ js/eslint.config.mjs
@@ -0,0 +1,157 @@
+import react from "eslint-plugin-react";
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+import reactHooks from "eslint-plugin-react-hooks";
+import prettierRecommended from "eslint-plugin-prettier/recommended";
+import globals from "globals";
+import importXPlugin from "eslint-plugin-import-x";
+import jsxA11y from "eslint-plugin-jsx-a11y";
+import tailwind from "eslint-plugin-tailwindcss";
+import nextPlugin from "@next/eslint-plugin-next";
+
+export default tseslint.config(
+  {
+    languageOptions: {
+      globals: {
+        ...globals.browser,
+        ...globals.jest,
+        ...globals.node,
+        ...globals.es2024,
+      },
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+        project: ["./tsconfig.json"],
+      },
+    },
+    settings: {
+      react: {
+        version: "detect",
+      },
+    },
+  },
+  eslint.configs.recommended,
+  importXPlugin.flatConfigs.recommended,
+  importXPlugin.flatConfigs.typescript,
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  ...tailwind.configs["flat/recommended"],
+  prettierRecommended,
+  {
+    ignores: [
+      "**/.next",
+      "*.js",
+      "**/.vscode",
+      "**/.husky",
+      "**/.github",
+      "**/public",
+      "**/node_modules",
+      "**/out",
+      "eslint.config.*",
+    ],
+  },
+  {
+    plugins: {
+      react,
+      "react-hooks": reactHooks,
+      "jsx-a11y": jsxA11y,
+      "@next/next": nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs["core-web-vitals"].rules,
+      ...reactHooks.configs.recommended.rules,
+      "import-x/default": "off",
+      "import-x/no-named-as-default": "off",
+      "import-x/no-named-as-default-member": "off",
+      "@next/next/no-html-link-for-pages": "warn",
+      "@next/next/no-img-element": "off",
+      "@next/next/no-duplicate-head": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "error",
+      "react/prop-types": "off",
+      "react/react-in-jsx-scope": "off",
+      // 'react/no-array-index-key': 'error',
+      // "react/no-unknown-property": [
+      //   "error",
+      //   {
+      //     ignore: ["jsx", "global"],
+      //   },
+      // ],
+      // '@typescript-eslint/no-explicit-any': 'error',
+      "@typescript-eslint/explicit-module-boundary-types": "off",
+      "@typescript-eslint/no-empty-function": "off",
+      "@typescript-eslint/non-nullable-type-assertion-style": "off",
+      "@typescript-eslint/no-unused-vars": "warn",
+      "@typescript-eslint/no-explicit-any": "warn",
+      // TODO
+      //  ------------------------------------------------------------------------------------
+      //  Marking the below as warnings ""for now"" - They need to be addressed in the future
+      //  ------------------------------------------------------------------------------------
+      "prettier/prettier": [
+        "warn",
+        {
+          semi: true,
+          endOfLine: "auto",
+          tabWidth: 2,
+          printWidth: 100,
+          singleQuote: false,
+          trailingComma: "all",
+          bracketSameLine: true,
+          usePrettierrc: false,
+        },
+      ],
+      "no-useless-escape": "warn",
+      "no-var": "warn",
+      "prefer-const": "warn",
+      "no-extra-boolean-cast": "warn",
+      "no-prototype-builtins": "warn",
+      "react/no-array-index-key": "warn",
+      "react/no-unknown-property": [
+        "warn",
+        {
+          ignore: ["jsx", "global"],
+        },
+      ],
+      "@typescript-eslint/no-unsafe-function-type": "warn",
+      "@typescript-eslint/no-extra-non-null-assertion": "warn",
+      "@typescript-eslint/array-type": "warn",
+      "@typescript-eslint/restrict-plus-operands": "warn",
+      "@typescript-eslint/no-inferrable-types": "warn",
+      "@typescript-eslint/no-unnecessary-type-arguments": "warn",
+      "@typescript-eslint/consistent-indexed-object-style": "warn",
+      "@typescript-eslint/no-deprecated": "warn",
+      "@typescript-eslint/no-unnecessary-boolean-literal-compare": "warn",
+      "@typescript-eslint/no-unnecessary-type-assertion": "warn",
+      "@typescript-eslint/no-empty-object-type": "warn",
+      "@typescript-eslint/dot-notation": "warn",
+      "@typescript-eslint/no-wrapper-object-types": "warn",
+      "@typescript-eslint/consistent-generic-constructors": "warn",
+      "@typescript-eslint/no-unnecessary-template-expression": "warn",
+      "@typescript-eslint/consistent-type-definitions": "warn",
+      "@typescript-eslint/no-unnecessary-condition": "warn",
+      "@typescript-eslint/no-unsafe-member-access": "warn",
+      "@typescript-eslint/no-misused-promises": "warn",
+      "@typescript-eslint/restrict-template-expressions": "warn",
+      "@typescript-eslint/prefer-nullish-coalescing": "warn",
+      "@typescript-eslint/no-non-null-assertion": "warn",
+      "@typescript-eslint/no-non-null-asserted-optional-chain": "warn",
+      "@typescript-eslint/no-floating-promises": "warn",
+      "@typescript-eslint/no-unsafe-assignment": "warn",
+      "@typescript-eslint/no-unsafe-argument": "warn",
+      "@typescript-eslint/no-unsafe-call": "warn",
+      "@typescript-eslint/no-redundant-type-constituents": "warn",
+      "@typescript-eslint/no-unsafe-return": "warn",
+      "@typescript-eslint/unbound-method": "warn",
+      "@typescript-eslint/no-unnecessary-type-parameters": "warn",
+      "@typescript-eslint/no-unused-expressions": "warn",
+      "@typescript-eslint/prefer-optional-chain": "warn",
+      "@typescript-eslint/require-await": "warn",
+      "@typescript-eslint/await-thenable": "warn",
+      "@typescript-eslint/no-confusing-void-expression": "warn",
+    },
+  },
+);
