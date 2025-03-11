c4ffa3d9a52379e0e7c877934cbc64d95718be38
Wei-Chun, Chang <wcchang@infuseai.io>
2025-03-11 15:11:38 +0800

    Review CLI message
    Signed-off-by: Wei-Chun, Chang <wcchang@infuseai.io>

--- recce/cli.py
+++ recce/cli.py
@@ -260,9 +260,9 @@ def server(host, port, state_file=None, **kwargs):
 
         # Show warning message
         console.rule('Notice', style='orange3')
-        console.print('Recce is launching in limited functionality mode.')
-        console.print('To use the full suite of Recce tools, please prepare the dbt artifacts to serve as the base for '
-                      f"your comparison in '{target_base_path}'.")
+        console.print('Recce is launching in single environment mode with limited functionality.')
+        console.print('For full functionality, prepare a base set of dbt artifacts to compare against in '
+                      f"'{target_base_path}'.")
         console.print('https://datarecce.io/docs/get-started/#prepare-dbt-artifacts')
         console.print()
 
