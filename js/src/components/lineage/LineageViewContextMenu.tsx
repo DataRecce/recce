15025eec9da22f443f1a9d753a2f79ec1807a498
popcorny <celu@infuseai.io>
2025-05-05 16:18:00 +0800

    Refactor
    Signed-off-by: popcorny <celu@infuseai.io>

--- js/src/components/lineage/LineageViewContextMenu.tsx
+++ js/src/components/lineage/LineageViewContextMenu.tsx
@@ -10,7 +10,6 @@ import { useLineageGraphContext } from "@/lib/hooks/LineageGraphContext";
 import { supportsHistogramDiff } from "../histogram/HistogramDiffForm";
 import { useRecceInstanceContext } from "@/lib/hooks/RecceInstanceContext";
 import { useRecceServerFlag } from "@/lib/hooks/useRecceServerFlag";
-import { memoryUsage } from "process";
 import useModelColumns from "@/lib/hooks/useModelColumns";
 import { useRecceQueryContext } from "@/lib/hooks/RecceQueryContext";
 import { useLocation } from "wouter";
@@ -23,26 +22,74 @@ interface LineageViewContextMenuProps {
   onClose: () => void;
 }
 
-export const LineageViewContextMenu = ({
+interface ContextMenuItem {
+  label?: string;
+  icon?: React.ReactElement;
+  action?: () => void;
+  isDisabled?: boolean;
+  isSeparator?: boolean;
+}
+
+interface ContextMenuProps {
+  menuItems: ContextMenuItem[];
+  isOpen: boolean;
+  onClose: () => void;
+  x: number;
+  y: number;
+}
+
+const ContextMenu = ({ menuItems, isOpen, onClose, x, y }: ContextMenuProps) => {
+  return (
+    <Menu isOpen={isOpen} onClose={onClose}>
+      <MenuList
+        fontSize="9pt"
+        position="absolute"
+        width="250px"
+        style={{
+          left: `${x}px`,
+          top: `${y}px`,
+        }}>
+        {menuItems.length === 0 ? (
+          <MenuItem isDisabled>No action available</MenuItem>
+        ) : (
+          menuItems.map((item) => {
+            if (item.isSeparator) {
+              return <MenuDivider />;
+            } else {
+              return (
+                <MenuItem
+                  icon={item.icon}
+                  isDisabled={item.isDisabled}
+                  onClick={() => {
+                    if (item.action) {
+                      item.action();
+                    }
+                    onClose();
+                  }}>
+                  {item.label}
+                </MenuItem>
+              );
+            }
+          })
+        )}
+      </MenuList>
+    </Menu>
+  );
+};
+
+export const ModelNodeContextMenu = ({
   isOpen,
   onClose,
   x,
   y,
   node,
 }: LineageViewContextMenuProps) => {
-  const menuItems: {
-    label?: string;
-    icon?: React.ReactElement;
-    action?: () => void;
-    isDisabled?: boolean;
-    isSeparator?: boolean;
-  }[] = [];
+  const menuItems: ContextMenuItem[] = [];
 
   const { selectParentNodes, selectChildNodes, getNodeColumnSet, selectMode } =
     useLineageViewContextSafe();
   const { runAction } = useRecceActionContext();
   const { isActionAvailable } = useLineageGraphContext();
-  const { readOnly } = useRecceInstanceContext();
   const { data: flag } = useRecceServerFlag();
   const singleEnv = flag?.single_env_onboarding ?? false;
 
@@ -51,203 +98,200 @@ export const LineageViewContextMenu = ({
   const { setSqlQuery, setPrimaryKeys } = useRecceQueryContext();
   const [, setLocation] = useLocation();
 
-  if (readOnly) {
-    // no items in the context menu
-    // we should prevent to show the context menu in the readonly mode
-  } else if (node?.type === "customNode") {
-    const modelNode = node.data as LineageGraphNode;
-    const resourceType = modelNode.resourceType;
+  if (!node?.data) {
+    return <></>;
+  }
 
-    if (!selectMode && resourceType && ["model", "seed", "snapshot"].includes(resourceType)) {
-      // query
-      let entry = findByRunType(singleEnv ? "query" : "query_diff");
-      menuItems.push({
-        label: "Query",
-        icon: <Icon as={entry?.icon} />,
-        action: () => {
-          setSqlQuery(`select * from {{ ref("${modelNode.name}") }}`);
-          if (isActionAvailable("query_diff_with_primary_key")) {
-            // Only set primary key if the action is available
-            setPrimaryKeys(primaryKey !== undefined ? [primaryKey] : undefined);
-          }
-          setLocation("/query");
-        },
-      });
+  const modelNode = node.data as LineageGraphNode;
+  const resourceType = modelNode.resourceType;
 
-      // row count & row count diff
-      entry = findByRunType(singleEnv ? "row_count" : "row_count_diff");
-      menuItems.push({
-        label: entry?.title ?? "Row count",
-        icon: <Icon as={entry?.icon} />,
-        action: () => {
-          runAction(
-            singleEnv ? "row_count" : "row_count_diff",
-            { node_names: [modelNode.name] },
-            { showForm: false },
-          );
-        },
-      });
+  if (!selectMode && resourceType && ["model", "seed", "snapshot"].includes(resourceType)) {
+    // query
+    let entry = findByRunType(singleEnv ? "query" : "query_diff");
+    menuItems.push({
+      label: "Query",
+      icon: <Icon as={entry?.icon} />,
+      action: () => {
+        setSqlQuery(`select * from {{ ref("${modelNode.name}") }}`);
+        if (isActionAvailable("query_diff_with_primary_key")) {
+          // Only set primary key if the action is available
+          setPrimaryKeys(primaryKey !== undefined ? [primaryKey] : undefined);
+        }
+        setLocation("/query");
+      },
+    });
 
-      // profile & profile diff
-      entry = findByRunType(singleEnv ? "profile" : "profile_diff");
-      menuItems.push({
-        label: entry?.title ?? "Profile",
-        icon: <Icon as={entry?.icon} />,
-        action: () => {
-          const columns = Array.from(getNodeColumnSet(node.id));
-          runAction(
-            singleEnv ? "profile" : "profile_diff",
-            { model: modelNode.name, columns },
-            { showForm: true },
-          );
-        },
-      });
+    // row count & row count diff
+    entry = findByRunType(singleEnv ? "row_count" : "row_count_diff");
+    menuItems.push({
+      label: entry?.title ?? "Row count",
+      icon: <Icon as={entry?.icon} />,
+      action: () => {
+        runAction(
+          singleEnv ? "row_count" : "row_count_diff",
+          { node_names: [modelNode.name] },
+          { showForm: false },
+        );
+      },
+    });
 
-      // value diff
-      if (!singleEnv) {
-        entry = findByRunType("value_diff");
-        menuItems.push({
-          label: entry?.title ?? "Value Diff",
-          icon: <Icon as={entry?.icon} />,
-          action: () => {
-            const columns = Array.from(getNodeColumnSet(node.id));
-            runAction("value_diff", { model: modelNode.name, columns }, { showForm: true });
-          },
-        });
-      }
-    }
-
-    if (!singleEnv) {
-      if (menuItems.length > 0) {
-        menuItems.push({
-          isSeparator: true,
-        });
-      }
-      menuItems.push({
-        label: "Select parent nodes",
-        icon: <BiArrowFromBottom />,
-        action: () => {
-          selectParentNodes(node.id, 1);
-        },
-      });
-      menuItems.push({
-        label: "Select child nodes",
-        icon: <BiArrowToBottom />,
-        action: () => {
-          selectChildNodes(node.id, 1);
-        },
-      });
-      menuItems.push({
-        label: "Select all upstream nodes",
-        icon: <BiArrowFromBottom />,
-        action: () => {
-          selectParentNodes(node.id);
-        },
-      });
-      menuItems.push({
-        label: "Select all downstream nodes",
-        icon: <BiArrowToBottom />,
-        action: () => {
-          selectChildNodes(node.id);
-        },
-      });
-    }
-  } else if (node?.type === "customColumnNode") {
-    const columnNode = node.data as LinageGraphColumnNode;
-    const modelNode = columnNode.node;
-    const column = columnNode.column;
-    const columnType = columnNode.type;
-
-    const handleProfileDiff = () => {
-      runAction("profile_diff", { model: modelNode.name, columns: [column] }, { showForm: false });
-    };
-
-    const handleHistogramDiff = () => {
-      runAction(
-        "histogram_diff",
-        { model: modelNode.name, column_name: column, column_type: columnType },
-        { showForm: false },
-      );
-    };
-
-    const handleTopkDiff = () => {
-      runAction(
-        "top_k_diff",
-        { model: modelNode.name, column_name: column, k: 50 },
-        { showForm: false },
-      );
-    };
-    const addedOrRemoved =
-      modelNode.data.base?.columns?.[column] === undefined ||
-      modelNode.data.current?.columns?.[column] === undefined;
-
-    let entry = findByRunType(singleEnv ? "profile" : "profile_diff");
+    // profile & profile diff
+    entry = findByRunType(singleEnv ? "profile" : "profile_diff");
     menuItems.push({
       label: entry?.title ?? "Profile",
       icon: <Icon as={entry?.icon} />,
-      action: handleProfileDiff,
-      isDisabled: addedOrRemoved || !isActionAvailable("profile_diff"),
+      action: () => {
+        const columns = Array.from(getNodeColumnSet(node.id));
+        runAction(
+          singleEnv ? "profile" : "profile_diff",
+          { model: modelNode.name, columns },
+          { showForm: true },
+        );
+      },
     });
 
+    // value diff
     if (!singleEnv) {
-      entry = findByRunType("histogram_diff");
+      entry = findByRunType("value_diff");
       menuItems.push({
-        label: entry?.title ?? "Histogram Diff",
+        label: entry?.title ?? "Value Diff",
         icon: <Icon as={entry?.icon} />,
-        action: handleHistogramDiff,
-        isDisabled: addedOrRemoved || (columnType ? !supportsHistogramDiff(columnType) : true),
-      });
-      entry = findByRunType("top_k_diff");
-      menuItems.push({
-        label: entry?.title ?? "Top-K Diff",
-        icon: <Icon as={entry?.icon} />,
-        action: handleTopkDiff,
-        isDisabled: addedOrRemoved,
+        action: () => {
+          const columns = Array.from(getNodeColumnSet(node.id));
+          runAction("value_diff", { model: modelNode.name, columns }, { showForm: true });
+        },
       });
     }
   }
 
-  return (
-    <>
-      {isOpen && (
-        // Only render context menu when select mode is action
-        <Menu isOpen={true} onClose={onClose}>
-          <MenuList
-            fontSize="9pt"
-            position="absolute"
-            width="250px"
-            style={{
-              left: `${x}px`,
-              top: `${y}px`,
-            }}>
-            {menuItems.length === 0 ? (
-              <MenuItem isDisabled>No action available</MenuItem>
-            ) : (
-              menuItems.map((item) => {
-                if (item.isSeparator) {
-                  return <MenuDivider />;
-                } else {
-                  return (
-                    <MenuItem
-                      icon={item.icon}
-                      isDisabled={item.isDisabled}
-                      onClick={() => {
-                        if (item.action) {
-                          item.action();
-                        }
-                        onClose();
-                      }}>
-                      {item.label}
-                    </MenuItem>
-                  );
-                }
-              })
-            )}
-          </MenuList>
-        </Menu>
-      )}
-    </>
-  );
+  if (!singleEnv) {
+    if (menuItems.length > 0) {
+      menuItems.push({
+        isSeparator: true,
+      });
+    }
+    menuItems.push({
+      label: "Select parent nodes",
+      icon: <BiArrowFromBottom />,
+      action: () => {
+        selectParentNodes(node.id, 1);
+      },
+    });
+    menuItems.push({
+      label: "Select child nodes",
+      icon: <BiArrowToBottom />,
+      action: () => {
+        selectChildNodes(node.id, 1);
+      },
+    });
+    menuItems.push({
+      label: "Select all upstream nodes",
+      icon: <BiArrowFromBottom />,
+      action: () => {
+        selectParentNodes(node.id);
+      },
+    });
+    menuItems.push({
+      label: "Select all downstream nodes",
+      icon: <BiArrowToBottom />,
+      action: () => {
+        selectChildNodes(node.id);
+      },
+    });
+  }
+
+  return <ContextMenu x={x} y={y} menuItems={menuItems} isOpen={isOpen} onClose={onClose} />;
+};
+
+export const ColumnNodeContextMenu = ({
+  isOpen,
+  onClose,
+  x,
+  y,
+  node,
+}: LineageViewContextMenuProps) => {
+  const menuItems: ContextMenuItem[] = [];
+
+  const { runAction } = useRecceActionContext();
+  const { isActionAvailable } = useLineageGraphContext();
+  const { data: flag } = useRecceServerFlag();
+  const singleEnv = flag?.single_env_onboarding ?? false;
+
+  if (node?.data === undefined) {
+    return <></>;
+  }
+
+  const columnNode = node.data as LinageGraphColumnNode;
+  const modelNode = columnNode.node;
+  const column = columnNode.column;
+  const columnType = columnNode.type;
+
+  const handleProfileDiff = () => {
+    runAction("profile_diff", { model: modelNode.name, columns: [column] }, { showForm: false });
+  };
+
+  const handleHistogramDiff = () => {
+    runAction(
+      "histogram_diff",
+      { model: modelNode.name, column_name: column, column_type: columnType },
+      { showForm: false },
+    );
+  };
+
+  const handleTopkDiff = () => {
+    runAction(
+      "top_k_diff",
+      { model: modelNode.name, column_name: column, k: 50 },
+      { showForm: false },
+    );
+  };
+  const addedOrRemoved =
+    modelNode.data.base?.columns?.[column] === undefined ||
+    modelNode.data.current?.columns?.[column] === undefined;
+
+  let entry = findByRunType(singleEnv ? "profile" : "profile_diff");
+  menuItems.push({
+    label: entry?.title ?? "Profile",
+    icon: <Icon as={entry?.icon} />,
+    action: handleProfileDiff,
+    isDisabled: addedOrRemoved || !isActionAvailable("profile_diff"),
+  });
+
+  if (!singleEnv) {
+    entry = findByRunType("histogram_diff");
+    menuItems.push({
+      label: entry?.title ?? "Histogram Diff",
+      icon: <Icon as={entry?.icon} />,
+      action: handleHistogramDiff,
+      isDisabled: addedOrRemoved || (columnType ? !supportsHistogramDiff(columnType) : true),
+    });
+    entry = findByRunType("top_k_diff");
+    menuItems.push({
+      label: entry?.title ?? "Top-K Diff",
+      icon: <Icon as={entry?.icon} />,
+      action: handleTopkDiff,
+      isDisabled: addedOrRemoved,
+    });
+  }
+
+  return <ContextMenu x={x} y={y} menuItems={menuItems} isOpen={isOpen} onClose={onClose} />;
+};
+
+export const LineageViewContextMenu = ({
+  isOpen,
+  onClose,
+  x,
+  y,
+  node,
+}: LineageViewContextMenuProps) => {
+  const { readOnly } = useRecceInstanceContext();
+  if (readOnly) {
+    <ContextMenu menuItems={[]} isOpen={isOpen} onClose={onClose} x={x} y={y} />;
+  } else if (node?.type === "customNode") {
+    return <ModelNodeContextMenu x={x} y={y} isOpen={isOpen} onClose={onClose} node={node} />;
+  } else if (node?.type === "customColumnNode") {
+    return <ColumnNodeContextMenu x={x} y={y} isOpen={isOpen} onClose={onClose} node={node} />;
+  }
 };
 
 export const useLineageViewContextMenu = ({
