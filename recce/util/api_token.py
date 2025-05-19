e7fff9783de7756a9657dfbc2dceba748575abd0
Kent Huang <kent@infuseai.io>
2025-05-19 17:17:57 +0800

    [UnitTest] Add unit test for api_token.py
    Signed-off-by: Kent Huang <kent@infuseai.io>

--- recce/util/api_token.py
+++ recce/util/api_token.py
@@ -1,10 +1,14 @@
 import click
-from click import Abort
 from rich.console import Console
 
 from recce.event import get_recce_api_token, update_recce_api_token
-from recce.util.recce_cloud import RecceCloud, get_recce_cloud_onboarding_state, set_recce_cloud_onboarding_state, \
-    RECCE_CLOUD_BASE_URL
+from recce.exceptions import RecceConfigException
+from recce.util.recce_cloud import (
+    RECCE_CLOUD_BASE_URL,
+    RecceCloud,
+    get_recce_cloud_onboarding_state,
+    set_recce_cloud_onboarding_state,
+)
 
 console = Console()
 
@@ -17,19 +21,21 @@ def show_invalid_api_token_message():
     console.print(f"Please check your API token from {RECCE_CLOUD_BASE_URL}/settings#tokens")
 
 
-def prepare_api_token(interaction=False, **kwargs, ):
+def prepare_api_token(
+    interaction=False,
+    **kwargs,
+):
     """
     Prepare the API token for the request.
     """
     # Verify the API token for Recce Cloud Share Link
     api_token = get_recce_api_token()
-    new_api_token = kwargs.get('api_token')
+    new_api_token = kwargs.get("api_token")
     if api_token != new_api_token and new_api_token is not None:
         # Handle the API token provided by option `--api-token`
         valid = RecceCloud(new_api_token).verify_token()
         if not valid:
-            show_invalid_api_token_message()
-            exit(1)
+            raise RecceConfigException("Invalid Recce Cloud API token")
         api_token = new_api_token
         update_recce_api_token(api_token)
         console.print("[[green]Success[/green]] Update the user profile for Recce Cloud API Token.")
@@ -47,15 +53,10 @@ def prepare_api_token(interaction=False, **kwargs, ):
                 f"{RECCE_CLOUD_BASE_URL}/settings#tokens\n"
                 "Your API token will be added to '~/.recce/profile.yml' for more convenient sharing."
             )
-            try:
-                api_token = click.prompt("Your Recce API token", type=str, hide_input=True, show_default=False)
-            except Abort:
-                console.print("[yellow]Aborted[/yellow]")
-                exit(1)
+            api_token = click.prompt("Your Recce API token", type=str, hide_input=True, show_default=False)
             valid = RecceCloud(api_token).verify_token()
             if not valid:
-                show_invalid_api_token_message()
-                exit(1)
+                raise RecceConfigException("Invalid Recce Cloud API token")
             update_recce_api_token(api_token)
             console.print("[[green]Success[/green]] Update the user profile for Recce Cloud API Token.")
 
