826494d3dee103114a22838d540604cb479c8258
popcorny <celu@infuseai.io>
2025-05-22 14:42:58 +0800

    Update the source parsing
    Signed-off-by: popcorny <celu@infuseai.io>

--- tests/adapter/dbt_adapter/test_dbt_cll.py
+++ tests/adapter/dbt_adapter/test_dbt_cll.py
@@ -45,23 +45,41 @@ def test_cll_basic(dbt_test_helper):
         curr_columns={"c": "int"},
         depends_on=["model.model1"],
     )
+    dbt_test_helper.create_model(
+        "model3",
+        unique_id="model.model3",
+        curr_sql='select c from {{ ref("recce_test", "model1") }} where c > 0',
+        curr_columns={"c": "int"},
+        depends_on=["model.model1"],
+    )
     adapter: DbtAdapter = dbt_test_helper.context.adapter
+
     result = adapter.get_cll_by_node_id("model.model2")
     assert_model(result, "model.model2", [("model.model1", "c")])
     assert_column(result, "model.model2", "c", "passthrough", [("model.model1", "c")])
+
+    result = adapter.get_cll_by_node_id("model.model3")
+    assert_model(result, "model.model3", [("model.model1", "c")])
+    assert_column(result, "model.model3", "c", "passthrough", [("model.model1", "c")])
 
 
 def test_cll_table_alisa(dbt_test_helper):
     def patch_node(node):
         node["alias"] = "model1_alias"
 
-    dbt_test_helper.create_model("model1", curr_sql="select 1 as c", curr_columns={"c": "int"}, patch_func=patch_node)
     dbt_test_helper.create_model(
-        "model2", curr_sql='select c from {{ ref("model1") }}', curr_columns={"c": "int"}, depends_on=["model1"]
+        "model1", unique_id="model.model1", curr_sql="select 1 as c", curr_columns={"c": "int"}, patch_func=patch_node
+    )
+    dbt_test_helper.create_model(
+        "model2",
+        unique_id="model.model2",
+        curr_sql='select c from {{ ref("model1") }}',
+        curr_columns={"c": "int"},
+        depends_on=["model.model1"],
     )
     adapter: DbtAdapter = dbt_test_helper.context.adapter
-    result = adapter.get_cll_by_node_id("model1")
-    assert_column(result, "model2", "c", "passthrough", [("model1", "c")])
+    result = adapter.get_cll_by_node_id("model.model1")
+    assert_column(result, "model.model2", "c", "passthrough", [("model.model1", "c")])
 
 
 def test_seed(dbt_test_helper):
@@ -74,13 +92,26 @@ def test_seed(dbt_test_helper):
 
     dbt_test_helper.create_model(
         "seed1",
+        unique_id="seed.seed1",
         curr_csv=csv_data_curr,
         curr_columns={"customer_id": "varchar", "name": "varchar", "age": "int"},
         resource_type="seed",
     )
+    dbt_test_helper.create_model(
+        "model1",
+        unique_id="model.model1",
+        curr_sql='select customer_id from {{ ref("seed1") }} where age > 0',
+        curr_columns={"customer_id": "varchar"},
+        depends_on=["seed.seed1"],
+    )
     adapter: DbtAdapter = dbt_test_helper.context.adapter
-    result = adapter.get_cll_by_node_id("seed1")
-    assert_column(result, "seed1", "customer_id", "source", [])
+    # result = adapter.get_cll_by_node_id("seed.seed1")
+
+    result = adapter.get_cll_by_node_id("model.model1")
+    assert_model(result, "seed.seed1", [])
+    assert_column(result, "seed.seed1", "customer_id", "source", [])
+    assert_model(result, "model.model1", [("seed.seed1", "age")])
+    assert_column(result, "model.model1", "customer_id", "passthrough", [("seed.seed1", "customer_id")])
 
 
 def test_python_model(dbt_test_helper):
@@ -122,20 +153,22 @@ def test_source(dbt_test_helper):
     dbt_test_helper.create_source(
         "source1",
         "table1",
+        unique_id="source.source1.table1",
         curr_csv=csv_data_curr,
         curr_columns={"customer_id": "varchar", "name": "varchar", "age": "int"},
     )
     dbt_test_helper.create_model(
         "model1",
+        unique_id="model.model1",
         curr_sql='select customer_id from {{ source("source1", "table1") }}',
         curr_columns={"customer_id": "int"},
-        depends_on=["source1.table1"],
+        depends_on=["source.source1.table1"],
     )
     adapter: DbtAdapter = dbt_test_helper.context.adapter
-    result = adapter.get_cll_by_node_id("source.recce_test.source1.table1")
-    assert_column(result, "source.recce_test.source1.table1", "customer_id", "source", [])
-    result = adapter.get_cll_by_node_id("model1")
-    assert_column(result, "model1", "customer_id", "passthrough", [("source.recce_test.source1.table1", "customer_id")])
+    result = adapter.get_cll_by_node_id("source.source1.table1")
+    assert_column(result, "source.source1.table1", "customer_id", "source", [])
+    result = adapter.get_cll_by_node_id("model.model1")
+    assert_column(result, "model.model1", "customer_id", "passthrough", [("source.source1.table1", "customer_id")])
 
 
 def test_parse_error(dbt_test_helper):
