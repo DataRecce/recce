957127a3112823a728b87fcba284037de8d48c24
popcornylu <popcornylu@users.noreply.github.com>
2025-06-15 23:58:59 +0000

    AUTO-COMMIT - build js statics to recce data/

---
+++ recce/data/_next/static/FZAswiR1IZs4v5X1L6nMo/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-e7650df18ca04bde.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
