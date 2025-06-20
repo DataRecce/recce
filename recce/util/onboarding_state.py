14da1275bedc9504c3aeaff6d7c4ad2b074bc2b3
Jared Scott <jared.scott@datarecce.io>
2025-06-20 16:51:39 +0800

    feat: drc-1338
    Adjust the OnboardingState names
    
    Signed-off-by: Jared Scott <jared.scott@datarecce.io>

--- recce/util/onboarding_state.py
+++ recce/util/onboarding_state.py
@@ -8,9 +8,9 @@ from recce.util.recce_cloud import (
 
 
 class OnboardingState(Enum):
-    NEW = "signup_successful"
-    LAUNCHED = "launched_with_token"
-    LAUNCHED_WITH_TWO_ENVS = "configure_two_env"
+    SIGNUP_SUCCESSFUL = "signup_successful"
+    LAUNCHED_WITH_TOKEN = "launched_with_token"
+    CONFIGURE_TWO_ENV = "configure_two_env"
 
 
 def update_onboarding_state(api_token: Union[str, bool, None], is_single_env: bool) -> OnboardingState:
@@ -21,25 +21,25 @@ def update_onboarding_state(api_token: Union[str, bool, None], is_single_env: bo
         #   launched -> launched_with_two_envs
         cloud_onboarding_state = get_recce_cloud_onboarding_state(api_token)
 
-        if cloud_onboarding_state == OnboardingState.NEW.value:
+        if cloud_onboarding_state == OnboardingState.SIGNUP_SUCCESSFUL.value:
             # User has an API Token and is a "new" user
             if is_single_env:
                 # Mark the onboarding state as "launched" if the user is new
-                set_recce_cloud_onboarding_state(api_token, OnboardingState.LAUNCHED.value)
-                return OnboardingState.LAUNCHED
+                set_recce_cloud_onboarding_state(api_token, OnboardingState.LAUNCHED_WITH_TOKEN.value)
+                return OnboardingState.LAUNCHED_WITH_TOKEN
             else:
-                set_recce_cloud_onboarding_state(api_token, OnboardingState.LAUNCHED_WITH_TWO_ENVS.value)
-                return OnboardingState.LAUNCHED_WITH_TWO_ENVS
-        elif cloud_onboarding_state == OnboardingState.LAUNCHED.value:
+                set_recce_cloud_onboarding_state(api_token, OnboardingState.CONFIGURE_TWO_ENV.value)
+                return OnboardingState.CONFIGURE_TWO_ENV
+        elif cloud_onboarding_state == OnboardingState.LAUNCHED_WITH_TOKEN.value:
             # User has an API Token and has Two Environments
             if is_single_env:
                 # Just return the current state
-                return OnboardingState.LAUNCHED
+                return OnboardingState.LAUNCHED_WITH_TOKEN
             else:
-                set_recce_cloud_onboarding_state(api_token, OnboardingState.LAUNCHED_WITH_TWO_ENVS.value)
-                return OnboardingState.LAUNCHED_WITH_TWO_ENVS
-        elif cloud_onboarding_state == OnboardingState.LAUNCHED_WITH_TWO_ENVS.value:
+                set_recce_cloud_onboarding_state(api_token, OnboardingState.CONFIGURE_TWO_ENV.value)
+                return OnboardingState.CONFIGURE_TWO_ENV
+        elif cloud_onboarding_state == OnboardingState.CONFIGURE_TWO_ENV.value:
             # Just return the current state
-            return OnboardingState.LAUNCHED_WITH_TWO_ENVS
+            return OnboardingState.CONFIGURE_TWO_ENV
 
-    return OnboardingState.NEW
+    return OnboardingState.SIGNUP_SUCCESSFUL
