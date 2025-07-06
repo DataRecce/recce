d6c69baa151a50f0616e02276d9fead9985b757b
Jared Scott <jared.scott@datarecce.io>
2025-07-06 20:11:27 +0800

    chore: Fix Query form navbar
    Signed-off-by: Jared Scott <jared.scott@datarecce.io>

--- js/src/components/query/QueryForm.tsx
+++ js/src/components/query/QueryForm.tsx
@@ -39,11 +39,11 @@ export const QueryForm = ({ defaultPrimaryKeys, onPrimaryKeysChange, ...prob }: 
 
   return (
     <Flex {...prob}>
-      <Field.Root m="4px 8px">
-        <Field.Label fontSize={"8pt"} margin={"0"}>
+      <Field.Root m="0 0.5rem" gap={0}>
+        <Field.Label fontSize="0.625rem" margin={0}>
           Diff with Primary Key(s) (suggested){" "}
           <Tooltip content={labelInfo} positioning={{ placement: "bottom-end" }}>
-            <PiInfo color="gray.600" size="3" />
+            <PiInfo color="gray.600" />
           </Tooltip>
         </Field.Label>
         <DropdownValuesInput
@@ -52,7 +52,7 @@ export const QueryForm = ({ defaultPrimaryKeys, onPrimaryKeysChange, ...prob }: 
           defaultValues={defaultPrimaryKeys}
           suggestionList={availableColumns}
           onValuesChange={onPrimaryKeysChange}
-          size="xs"
+          size="2xs"
           width={"240px"}
           placeholder="Select or type to add keys"
           disabled={!isActionAvailable("query_diff_with_primary_key")}
