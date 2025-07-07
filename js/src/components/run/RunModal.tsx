e82ab71cca167d426351dde7881fc405ce178ac0
Jared Scott <jared.scott@datarecce.io>
2025-07-07 09:42:45 +0800

    chore: Fix the Popups for the Run Actions
    Signed-off-by: Jared Scott <jared.scott@datarecce.io>

--- js/src/components/run/RunModal.tsx
+++ js/src/components/run/RunModal.tsx
@@ -55,78 +55,82 @@ export const RunModal = <PT,>({
     <Dialog.Root open={isOpen} onOpenChange={onClose} size="xl" scrollBehavior="inside">
       <Portal>
         <Dialog.Backdrop />
-        <Dialog.Content overflowY="auto" height="75%" minHeight={"400px"}>
-          <Dialog.Header className="flex items-center">
-            {title}{" "}
-            {documentationUrl && (
-              <Popover.Root
-                positioning={{ placement: "bottom-end" }}
-                open={hovered}
-                onFocusOutside={() => {
-                  setHovered(false);
-                }}>
-                <Popover.Trigger asChild>
-                  <IconButton
-                    display="flex"
-                    size="sm"
-                    variant="plain"
-                    aria-label="Click this button to learn more about the SQL behind"
-                    onMouseEnter={() => {
-                      setHovered(true);
-                    }}
-                    onClick={() => window.open(documentationUrl, "_blank")}
-                    onFocus={(e) => {
-                      e.preventDefault();
+        <Dialog.Positioner>
+          <Dialog.Content overflowY="auto" height="75%" minHeight={"400px"}>
+            <Dialog.Header>
+              <Dialog.Title display="flex" alignItems="center">
+                {title}{" "}
+                {documentationUrl && (
+                  <Popover.Root
+                    positioning={{ placement: "bottom-end" }}
+                    open={hovered}
+                    onFocusOutside={() => {
+                      setHovered(false);
                     }}>
-                    <Icon verticalAlign="middle" as={IconInfo} boxSize={"16px"} />
-                  </IconButton>
-                </Popover.Trigger>
-                <Popover.Positioner>
-                  <Popover.Content bg="black" color="white">
-                    <Popover.Body fontSize="sm" p={2}>
-                      Click{" "}
-                      <Link
-                        href={documentationUrl}
-                        target="_blank"
-                        textDecoration="underline"
-                        color="white"
-                        _hover={{ color: "blue.300" }}>
-                        here
-                      </Link>{" "}
-                      to learn more about the SQL behind
-                    </Popover.Body>
-                  </Popover.Content>
-                </Popover.Positioner>
-              </Popover.Root>
-            )}
-          </Dialog.Header>
-          <Dialog.Body p="0px" h="100%" overflow="auto" borderY="1px solid lightgray">
-            <Box style={{ contain: "layout" }}>
-              {RunForm && (
-                <RunForm
-                  params={params}
-                  onParamsChanged={setParams}
-                  setIsReadyToExecute={setIsReadyToExecute}
-                />
-              )}
-            </Box>
-          </Dialog.Body>
-          <Dialog.Footer>
-            <Flex gap="10px">
-              <Button
-                disabled={!isReadyToExecute}
-                colorPalette="blue"
-                onClick={() => {
-                  onExecute(type, params as PT);
-                }}>
-                Execute
-              </Button>
-            </Flex>
-          </Dialog.Footer>
-          <Dialog.CloseTrigger asChild>
-            <CloseButton size="sm" />
-          </Dialog.CloseTrigger>
-        </Dialog.Content>
+                    <Popover.Trigger asChild>
+                      <IconButton
+                        display="flex"
+                        size="sm"
+                        variant="plain"
+                        aria-label="Click this button to learn more about the SQL behind"
+                        onMouseEnter={() => {
+                          setHovered(true);
+                        }}
+                        onClick={() => window.open(documentationUrl, "_blank")}
+                        onFocus={(e) => {
+                          e.preventDefault();
+                        }}>
+                        <Icon verticalAlign="middle" as={IconInfo} boxSize={"16px"} />
+                      </IconButton>
+                    </Popover.Trigger>
+                    <Popover.Positioner>
+                      <Popover.Content bg="black" color="white">
+                        <Popover.Body fontSize="sm" p={2}>
+                          Click{" "}
+                          <Link
+                            href={documentationUrl}
+                            target="_blank"
+                            textDecoration="underline"
+                            color="white"
+                            _hover={{ color: "blue.300" }}>
+                            here
+                          </Link>{" "}
+                          to learn more about the SQL behind
+                        </Popover.Body>
+                      </Popover.Content>
+                    </Popover.Positioner>
+                  </Popover.Root>
+                )}
+              </Dialog.Title>
+              <Dialog.CloseTrigger asChild>
+                <CloseButton size="sm" />
+              </Dialog.CloseTrigger>
+            </Dialog.Header>
+            <Dialog.Body p="0px" h="100%" overflow="auto" borderY="1px solid lightgray">
+              <Box style={{ contain: "layout" }}>
+                {RunForm && (
+                  <RunForm
+                    params={params}
+                    onParamsChanged={setParams}
+                    setIsReadyToExecute={setIsReadyToExecute}
+                  />
+                )}
+              </Box>
+            </Dialog.Body>
+            <Dialog.Footer>
+              <Flex gap="10px">
+                <Button
+                  disabled={!isReadyToExecute}
+                  colorPalette="blue"
+                  onClick={() => {
+                    onExecute(type, params as PT);
+                  }}>
+                  Execute
+                </Button>
+              </Flex>
+            </Dialog.Footer>
+          </Dialog.Content>
+        </Dialog.Positioner>
       </Portal>
     </Dialog.Root>
   );
