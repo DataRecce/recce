a7178401e280d57cfc355f5cab6770cb4ac937d2
kentwelcome <kentwelcome@users.noreply.github.com>
2025-07-30 11:20:52 +0000

    AUTO-COMMIT - build js statics to recce data/

---
+++ recce/data/_next/static/chunks/pages/_app-73008661edbd5e05.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{71720:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(82916)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(71720),n(26498))),_N_E=_.O()}]);
