cf3d8407caeed22252d2a4748ed4f890a4573ae0
Jared Scott <jared.scott@datarecce.io>
2025-08-06 11:35:37 +0800

    Feat: DRC-1572
    - Disable the Impact Radius button when the catalog.json file is not provided
    - Add a tooltip to explain the catalog.json file is necessary to enable Impact Radius
    - Ensure the Context menu item for Impact Radius is also disabled when catalog.json is not provided
    - Update packages to latest
    - Fix lint errors due to update
    - Switch `dev` to use `--turbopack`
     - First Compilation time is decreased by 70%!
    
    Signed-off-by: Jared Scott <jared.scott@datarecce.io>

--- js/src/components/lineage/LineageViewContextMenu.tsx
+++ js/src/components/lineage/LineageViewContextMenu.tsx
@@ -108,6 +108,8 @@ export const ModelNodeContextMenu = ({
   const { featureToggles } = useRecceInstanceContext();
   const { isActionAvailable } = useLineageGraphContext();
   const { data: flag } = useRecceServerFlag();
+  const { lineageGraph } = useLineageGraphContext();
+  const noCatalogCurrent = !lineageGraph?.catalogMetadata.current;
   const singleEnv = flag?.single_env_onboarding ?? false;
   const isQueryDisabled = featureToggles.disableDatabaseQuery;
 
@@ -132,6 +134,7 @@ export const ModelNodeContextMenu = ({
     menuItems.push({
       label: "Show Impact Radius",
       itemIcon: <FaRegDotCircle />,
+      isDisabled: noCatalogCurrent,
       action: () => {
         void showColumnLevelLineage({
           node_id: node.id,
