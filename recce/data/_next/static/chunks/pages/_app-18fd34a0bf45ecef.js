e7b7be0046fd4582041c7ee526b7c344c0e049b5
popcornylu <popcornylu@users.noreply.github.com>
2025-10-08 09:44:18 +0000

    AUTO-COMMIT - build js statics to recce data/

---
+++ recce/data/_next/static/chunks/pages/_app-18fd34a0bf45ecef.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[636],{16382:(_,n,p)=>{(window.__NEXT_P=window.__NEXT_P||[]).push(["/_app",function(){return p(27759)}])}},_=>{var n=n=>_(_.s=n);_.O(0,[593,792],()=>(n(16382),n(23159))),_N_E=_.O()}]);
