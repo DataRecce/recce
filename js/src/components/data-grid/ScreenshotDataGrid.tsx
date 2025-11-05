2ca6590cf80e6fad1b6971fe76aaee5dd70fbb3a
Jared Scott <jared.scott@datarecce.io>
2025-11-05 17:04:24 +0800

    feat: Begin fixing eslint warnings
    - Add explicit ref
    - Ignore <any> on DataGridProps (there are a lot of issues)
    
    Signed-off-by: Jared Scott <jared.scott@datarecce.io>

--- js/src/components/data-grid/ScreenshotDataGrid.tsx
+++ js/src/components/data-grid/ScreenshotDataGrid.tsx
@@ -1,20 +1,23 @@
 import "react-data-grid/lib/styles.css";
-import { DataGrid, DataGridProps } from "react-data-grid";
+import { DataGrid, DataGridHandle, DataGridProps } from "react-data-grid";
 import { Flex, Text } from "@chakra-ui/react";
-import { forwardRef } from "react";
+import React, { forwardRef } from "react";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 type ScreenshotDataGridProps = DataGridProps<any>;
 
-export const ScreenshotDataGrid = forwardRef(({ ...props }: ScreenshotDataGridProps, ref: any) => {
-  return (
-    <DataGrid
-      ref={ref}
-      className={props.className ? props.className + "no-track-pii-safe" : "no-track-pii-safe"}
-      rowClass={() => "no-track-pii-safe"}
-      {...props}
-    />
-  );
-});
+export const ScreenshotDataGrid = forwardRef(
+  ({ ...props }: ScreenshotDataGridProps, ref: React.Ref<DataGridHandle>) => {
+    return (
+      <DataGrid
+        ref={ref}
+        className={props.className ? props.className + "no-track-pii-safe" : "no-track-pii-safe"}
+        rowClass={() => "no-track-pii-safe"}
+        {...props}
+      />
+    );
+  },
+);
 
 interface EmptyRowsRendererProps {
   emptyMessage?: string;
