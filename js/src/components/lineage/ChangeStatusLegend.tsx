88e6077bd273b7f16f9a28faef220a6568f8534c
Jared Scott <jared.scott@datarecce.io>
2025-11-05 23:21:45 +0800

    feat: Fix the typing in ChangeStatusLegend.tsx
    Signed-off-by: Jared Scott <jared.scott@datarecce.io>

--- js/src/components/lineage/ChangeStatusLegend.tsx
+++ js/src/components/lineage/ChangeStatusLegend.tsx
@@ -2,8 +2,10 @@ import { Box, Flex, Icon } from "@chakra-ui/react";
 import { Tooltip } from "@/components/ui/tooltip";
 import { getIconForChangeStatus } from "./styles";
 
+type ChangeStatus = "added" | "removed" | "modified";
+
 export function ChangeStatusLegend() {
-  const CHANGE_STATUS_MSGS: Record<string, [string, string]> = {
+  const CHANGE_STATUS_MSGS: Record<ChangeStatus, [string, string]> = {
     added: ["Added", "Added resource"],
     removed: ["Removed", "Removed resource"],
     modified: ["Modified", "Modified resource"],
@@ -11,17 +13,19 @@ export function ChangeStatusLegend() {
 
   return (
     <Box bg="white" padding="12px" borderWidth="1px" borderColor="gray.200" fontSize="sm">
-      {Object.entries(CHANGE_STATUS_MSGS).map(([key, [label, tip]]) => {
-        const { icon, color } = getIconForChangeStatus(key as any);
+      {(Object.entries(CHANGE_STATUS_MSGS) as [ChangeStatus, [string, string]][]).map(
+        ([key, [label, tip]]) => {
+          const { icon, color } = getIconForChangeStatus(key);
 
-        return (
-          <Tooltip content={tip} key={key} positioning={{ placement: "right" }}>
-            <Flex alignItems="center" gap="6px" marginBottom="2px">
-              <Icon color={color} as={icon} /> {label}
-            </Flex>
-          </Tooltip>
-        );
-      })}
+          return (
+            <Tooltip content={tip} key={key} positioning={{ placement: "right" }}>
+              <Flex alignItems="center" gap="6px" marginBottom="2px">
+                <Icon color={color} as={icon} /> {label}
+              </Flex>
+            </Tooltip>
+          );
+        },
+      )}
     </Box>
   );
 }
