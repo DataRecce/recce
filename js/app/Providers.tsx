9228f9bc239a0a8e54b382622e3f77665c3393dc
Jared Scott <jared.scott@datarecce.io>
2025-12-02 22:35:01 +0800

    feat: remove HashbangRedirect.tsx
    - Cleanup Providers.tsx
    
    Signed-off-by: Jared Scott <jared.scott@datarecce.io>

--- js/app/Providers.tsx
+++ js/app/Providers.tsx
@@ -9,7 +9,6 @@
 
 import { QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
-import { HashbangRedirect } from "@/components/routing/HashbangRedirect";
 import { Provider } from "@/components/ui/provider";
 import { Toaster } from "@/components/ui/toaster";
 import { reactQueryClient } from "@/lib/api/axiosClient";
