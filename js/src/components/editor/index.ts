6855cec9b3cee1f0f7fe78490d32f74b6143a0af
Jared Scott <jared.scott@datarecce.io>
2025-12-12 14:47:27 +0800

    refactor: Replace Monaco Editor with CodeMirror
    Signed-off-by: Jared Scott <jared.scott@datarecce.io>

---
+++ js/src/components/editor/index.ts
@@ -0,0 +1,10 @@
+export {
+  CodeEditor,
+  type CodeEditorLanguage,
+  type CodeEditorProps,
+} from "./CodeEditor";
+export {
+  DiffEditor,
+  type DiffEditorLanguage,
+  type DiffEditorProps,
+} from "./DiffEditor";
