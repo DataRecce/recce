5da93afbce283b66a4c0e3b7f025df97fd74c30b
Jared Scott <jared.scott@datarecce.io>
2025-12-12 15:06:15 +0800

    refactor: Add better themeing
    Signed-off-by: Jared Scott <jared.scott@datarecce.io>

--- js/src/components/editor/index.ts
+++ js/src/components/editor/index.ts
@@ -2,9 +2,11 @@ export {
   CodeEditor,
   type CodeEditorLanguage,
   type CodeEditorProps,
+  type CodeEditorTheme,
 } from "./CodeEditor";
 export {
   DiffEditor,
   type DiffEditorLanguage,
   type DiffEditorProps,
+  type DiffEditorTheme,
 } from "./DiffEditor";
