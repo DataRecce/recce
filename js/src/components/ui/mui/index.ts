62e779ff734a2cb86f10f0d59427b7b177cb3727
Jared Scott <jared.scott@datarecce.io>
2025-12-15 19:10:12 +0800

    refactor(ui): Remove a majority of the migration files
    Signed-off-by: Jared Scott <jared.scott@datarecce.io>

--- js/src/components/ui/mui/index.ts
+++ js/src/components/ui/mui/index.ts
@@ -13,81 +13,8 @@
 export { type UseDisclosureReturn, useDisclosure } from "../mui-utils";
 // Layout Components
 export { Box, type BoxProps } from "./Box";
-// Navigation Components
-export {
-  Breadcrumb,
-  BreadcrumbItem,
-  type BreadcrumbItemProps,
-  BreadcrumbLink,
-  type BreadcrumbLinkProps,
-  BreadcrumbRoot,
-  type BreadcrumbRootProps,
-  BreadcrumbSeparator,
-} from "./Breadcrumb";
-export { Center, type CenterProps } from "./Center";
-// Form Components
-export { Checkbox, type CheckboxProps } from "./Checkbox";
-export { CloseButton, type CloseButtonProps } from "./CloseButton";
-export { Code, type CodeProps } from "./Code";
 // Overlay Components
-export {
-  Dialog,
-  DialogActionTrigger,
-  type DialogActionTriggerProps,
-  DialogBody,
-  type DialogBodyProps,
-  DialogCloseTrigger,
-  type DialogCloseTriggerProps,
-  DialogFooter,
-  type DialogFooterProps,
-  DialogHeader,
-  type DialogHeaderProps,
-  DialogRoot,
-  type DialogRootProps,
-  DialogTitle,
-  type DialogTitleProps,
-} from "./Dialog";
-export { Divider, type DividerProps, Separator } from "./Divider";
-export {
-  Field,
-  FieldErrorText,
-  FieldHelperText,
-  FieldLabel,
-  type FieldProps,
-} from "./Field";
 export { Flex, type FlexProps } from "./Flex";
-export { Grid, GridItem, type GridItemProps, type GridProps } from "./Grid";
-// Typography Components
-export { Heading, type HeadingProps } from "./Heading";
-export { Highlight, type HighlightProps } from "./Highlight";
-export { Icon, type IconProps } from "./Icon";
-export { IconButton, type IconButtonProps } from "./IconButton";
-export { Image, type ImageProps } from "./Image";
-export { Input, type InputProps } from "./Input";
-export { InputGroup, type InputGroupProps } from "./InputGroup";
-export { Link, type LinkProps } from "./Link";
-export {
-  List,
-  ListIcon,
-  ListItem,
-  type ListItemProps,
-  ListItemText,
-  type ListProps,
-} from "./List";
-export {
-  Menu,
-  MenuContent,
-  type MenuContentProps,
-  MenuItem,
-  type MenuItemGroupProps,
-  type MenuItemProps,
-  MenuRoot,
-  type MenuRootProps,
-  MenuSeparator,
-  MenuTrigger,
-  type MenuTriggerProps,
-} from "./Menu";
-export { NativeSelect } from "./NativeSelect";
 export {
   Popover,
   PopoverBody,
@@ -100,35 +27,6 @@ export {
   type PopoverTriggerProps,
 } from "./Popover";
 export { Portal, type PortalProps } from "./Portal";
-export {
-  ProgressCircle,
-  ProgressCircleCircle,
-  ProgressCircleRange,
-  ProgressCircleRoot,
-  type ProgressCircleRootProps,
-  ProgressCircleTrack,
-} from "./ProgressCircle";
-export {
-  RadioGroup,
-  RadioGroupItem,
-  RadioGroupItemHiddenInput,
-  RadioGroupItemIndicator,
-  type RadioGroupItemProps,
-  RadioGroupItemText,
-  type RadioGroupItemTextProps,
-  RadioGroupRoot,
-  type RadioGroupRootProps,
-} from "./RadioGroup";
-export { Select, SelectItem, type SelectProps } from "./Select";
-export { SimpleGrid, type SimpleGridProps } from "./SimpleGrid";
-export {
-  Skeleton,
-  SkeletonCircle,
-  type SkeletonCircleProps,
-  type SkeletonProps,
-  SkeletonText,
-  type SkeletonTextProps,
-} from "./Skeleton";
 export { Spacer } from "./Spacer";
 export { Spinner, type SpinnerProps } from "./Spinner";
 export {
@@ -139,24 +37,6 @@ export {
   type StackSeparatorProps,
   VStack,
 } from "./Stack";
-export { Switch, type SwitchProps } from "./Switch";
-export {
-  Table,
-  TableBody,
-  type TableBodyProps,
-  TableCell,
-  type TableCellProps,
-  TableColumnHeader,
-  type TableColumnHeaderProps,
-  TableHeader,
-  type TableHeaderProps,
-  TableRoot,
-  type TableRootProps,
-  TableRow,
-  type TableRowProps,
-  TableScrollArea,
-  type TableScrollAreaProps,
-} from "./Table";
 export {
   TabContent,
   type TabContentProps,
@@ -170,6 +50,4 @@ export {
   type TabTriggerProps,
 } from "./Tabs";
 export { Text, type TextProps } from "./Text";
-export { Textarea, type TextareaProps } from "./Textarea";
 export { Tooltip, type TooltipProps } from "./Tooltip";
-export { Wrap, WrapItem, type WrapItemProps, type WrapProps } from "./Wrap";
