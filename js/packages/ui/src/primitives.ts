eeb572fccfedf582b20e7d6d57500751f53f7f18
Jared Scott <jared.scott@datarecce.io>
2026-01-02 10:59:20 +0800

    feat(ui): add primitives entry point for building block components
    Signed-off-by: Jared Scott <jared.scott@datarecce.io>

---
+++ js/packages/ui/src/primitives.ts
@@ -0,0 +1,21 @@
+// @datarecce/ui/primitives - Building block components for custom composition
+// These are pure presentation components - no data fetching, just props and callbacks.
+
+"use client";
+
+export const PRIMITIVES_API_VERSION = "0.1.0";
+
+// Lineage primitives (to be added in subsequent tasks)
+// export { LineageNode } from "./components/lineage/LineageNode";
+// export { LineageEdge } from "./components/lineage/LineageEdge";
+// export { LineageControls } from "./components/lineage/LineageControls";
+
+// Check primitives (to be added)
+// export { CheckList } from "./components/check/CheckList";
+// export { CheckCard } from "./components/check/CheckCard";
+
+// Data primitives (to be added)
+// export { DataGrid } from "./components/data/DataGrid";
+
+// Placeholder export to prevent empty module error
+export {};
