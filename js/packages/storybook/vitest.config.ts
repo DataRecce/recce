8248967dd304cc6a57f347fb60b5a8fdef86caa6
Jared Scott <jared.scott@datarecce.io>
2026-01-16 20:04:18 +0800

    feat: add Vitest config for storybook package
    Signed-off-by: Jared Scott <jared.scott@datarecce.io>

---
+++ js/packages/storybook/vitest.config.ts
@@ -0,0 +1,13 @@
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    setupFiles: ["./.storybook/vitest.setup.ts"],
+    include: ["stories/**/*.test.ts"],
+    exclude: ["node_modules"],
+    globals: true,
+  },
+});
